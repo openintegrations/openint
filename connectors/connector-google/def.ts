c134e764a4cc0e6709c0ad7ca54abddf90740e36
Amadeo Pellicce <pellicceama@gmail.com>
2024-11-04 15:55:43 -0800

    removing default google connector config

--- connectors/connector-google/def.ts
+++ connectors/connector-google/def.ts
@@ -13,17 +13,17 @@ export const googleSchemas = {
   name: z.literal('google'),
   connectorConfig: z.object({
     oauth: z
-      .union([
-        // TODO: This should be z.literal('default') but it does not render well in the UI :/
-        z.null().openapi({title: 'Use OpenInt platform credentials'}),
-        z
-          .object({
-            client_id: z.string(),
-            client_secret: z.string(),
-            scopes: z.string().describe('comma seperated scopes'),
-          })
-          .openapi({title: 'Use my own'}),
-      ])
+      // .union([
+      //   // TODO: This should be z.literal('default') but it does not render well in the UI :/
+      //   z.null().openapi({title: 'Use OpenInt platform credentials'}),
+      // z
+      .object({
+        client_id: z.string(),
+        client_secret: z.string(),
+        scopes: z.string().describe('comma seperated scopes'),
+      })
+      //     .openapi({title: 'Use my own'}),
+      // ])
       .optional(),
   }),
   resourceSettings: zSettings,
