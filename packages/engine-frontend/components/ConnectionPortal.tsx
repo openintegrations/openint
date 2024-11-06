b115a5f7d794d2ac4371f709c9d5903720a5cbc2
Amadeo Pellicce <pellicceama@gmail.com>
2024-11-06 13:15:27 -0800

    WIP

--- packages/engine-frontend/components/ConnectionPortal.tsx
+++ packages/engine-frontend/components/ConnectionPortal.tsx
@@ -79,6 +79,30 @@ export function ConnectionPortal({className}: ConnectionPortalProps) {
             category.connectorConfigs.includes(c.connectorConfig),
           ),
         }))
+
+        // TODO: add list interations filtering and
+        // show integrations instead of connectors in children
+
+        // const listIntegrationsRes = _trpcReact.listConfiguredIntegrations.useQuery({
+        //   connector_config_ids: connectorConfigs.map((ccfg) => ccfg.id),
+        //   search_text: searchText,
+        // })
+        // const ints = listIntegrationsRes.data?.items
+        //   .map((int) => ({
+        //     ...int,
+        //     ccfg: connectorConfigs.find(
+        //       (ccfg) => ccfg.id === int.connector_config_id,
+        //     )!,
+        //   }))
+        //   .filter((int) => {
+        //     if (int.connector_name === 'google') {
+        //       // these are nested integrations within a connector i.e. int_google_gmail
+        //       // so we only want to show those enabled in the integrations list of the connector config
+        //       return int.ccfg.integrations.some((i) => int.id.includes(i))
+        //     }
+        //     return true
+        //   })
+
         const connectionCount = connections.length
 
         const tabConfig = [
