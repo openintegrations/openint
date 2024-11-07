193f4ad1f1ff081d38d199b1c91e2284b5f0c1c2
Rodrigo Arze Leon <rodrigo.arze@Rodrigos-Mac-mini.local>
2024-11-07 12:44:49 -0400

    Add tooltip to connection card

--- packages/ui/domain-components/ConnectionCard.tsx
+++ packages/ui/domain-components/ConnectionCard.tsx
@@ -8,6 +8,12 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '../shadcn'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../shadcn/Tooltip'
 import {ConnectorLogo} from './ConnectorCard'
 
 export function ConnectionCard({
@@ -23,58 +29,74 @@ export function ConnectionCard({
   }
 }) {
   return (
-    <Card className="relative h-[150px] w-[150px] cursor-pointer rounded-lg border border-gray-300 bg-white p-0">
-      <CardContent className="flex h-full flex-col items-center justify-center p-4">
-        <div className="flex size-full flex-col items-center justify-center gap-1">
-          <div className="flex w-full flex-row justify-between">
-            <AlertCircle
-              fill={conn.syncInProgress === undefined ? '#FF9500' : 'white'}
-              stroke="white"
-            />
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <CircleEllipsis className="size-5 self-end text-[#808080]" />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="flex w-[80px] items-center justify-center">
-                <DropdownMenuItem
-                  className="flex items-center justify-center"
-                  onSelect={() => onDelete({id: conn.id})}>
-                  <span className="text-center font-medium text-red-500">
-                    Delete
-                  </span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <ConnectorLogo
-            connector={conn.connectorConfig.connector}
-            className="size-[64px] rounded-lg"
-          />
-          <p
-            className={`m-0 max-w-[100px] text-center text-sm font-semibold ${
-              conn.connectorConfig.connector.displayName.length > 15
-                ? 'truncate'
-                : ''
-            }`}>
-            {conn.connectorConfig.connector.displayName
-              .charAt(0)
-              .toUpperCase() +
-              conn.connectorConfig.connector.displayName.slice(1)}
-          </p>
-          {conn.pipelineIds.length > 0 && (
-            <div>
-              {conn.syncInProgress ? (
-                <div className="flex flex-row items-center justify-start gap-2">
-                  <Loader className="size-5 animate-spin text-button" />
-                  <p className="text-center text-sm text-button">Syncing...</p>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger className="flex size-full w-[150px] flex-col gap-1">
+          <Card className="relative h-[150px] w-[150px] cursor-pointer rounded-lg border border-gray-300 bg-white p-0">
+            <CardContent className="flex h-full flex-col items-center justify-center p-4 py-2">
+              <div className="flex size-full flex-col items-center justify-center gap-1">
+                <div className="flex w-full flex-row justify-between">
+                  <AlertCircle
+                    fill={
+                      conn.syncInProgress === undefined ? '#FF9500' : 'white'
+                    }
+                    stroke="white"
+                  />
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <CircleEllipsis className="size-5 self-end text-[#808080]" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="flex w-[80px] items-center justify-center">
+                      <DropdownMenuItem
+                        className="flex items-center justify-center"
+                        onSelect={() => onDelete({id: conn.id})}>
+                        <span className="text-center font-medium text-red-500">
+                          Delete
+                        </span>
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
-              ) : (
-                <p className="text-center text-sm text-[#808080]">Synced</p>
-              )}
-            </div>
-          )}
-        </div>
-      </CardContent>
-    </Card>
+                <ConnectorLogo
+                  connector={conn.connectorConfig.connector}
+                  className="size-[64px] rounded-lg"
+                />
+                <p
+                  className={`m-0 max-w-[100px] text-center text-sm font-semibold ${
+                    conn.connectorConfig.connector.displayName.length > 15
+                      ? 'truncate'
+                      : ''
+                  }`}>
+                  {conn.connectorConfig.connector.displayName
+                    .charAt(0)
+                    .toUpperCase() +
+                    conn.connectorConfig.connector.displayName.slice(1)}
+                </p>
+                {conn.pipelineIds.length > 0 && (
+                  <div>
+                    {conn.syncInProgress ? (
+                      <div className="flex flex-row items-center justify-start gap-2">
+                        <Loader className="size-5 animate-spin text-button" />
+                        <p className="text-center text-sm text-button">
+                          Syncing...
+                        </p>
+                      </div>
+                    ) : (
+                      <p className="text-center text-sm text-[#808080]">
+                        Synced
+                      </p>
+                    )}
+                  </div>
+                )}
+              </div>
+            </CardContent>
+          </Card>
+        </TooltipTrigger>
+        <TooltipContent>
+          {conn.connectorConfig.connector.displayName.charAt(0).toUpperCase() +
+            conn.connectorConfig.connector.displayName.slice(1)}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
