869285fa34e92d8a8431de3d2664b38ba1f6d7ea
Rodrigo Arze Leon <rodrigo.arze@Rodrigos-Mac-mini.local>
2024-11-07 13:14:34 -0400

    Fix connection card ellipsis alignment in all cards

--- packages/ui/domain-components/ConnectionCard.tsx
+++ packages/ui/domain-components/ConnectionCard.tsx
@@ -1,4 +1,4 @@
-import {AlertCircle, CircleEllipsis, Loader} from 'lucide-react'
+import {CircleEllipsis, Loader} from 'lucide-react'
 import type {ConnectorConfig} from '../../engine-frontend/hocs/WithConnectConfig'
 import {
   Card,
@@ -34,29 +34,22 @@ export function ConnectionCard({
         <TooltipTrigger className="flex size-full w-[150px] flex-col gap-1">
           <Card className="relative h-[150px] w-[150px] cursor-pointer rounded-lg border border-gray-300 bg-white p-0">
             <CardContent className="flex h-full flex-col items-center justify-center p-4 py-2">
-              <div className="flex size-full flex-col items-center justify-center gap-1">
-                <div className="flex w-full flex-row justify-between">
-                  <AlertCircle
-                    fill={
-                      conn.syncInProgress === undefined ? '#FF9500' : 'white'
-                    }
-                    stroke="white"
-                  />
-                  <DropdownMenu>
-                    <DropdownMenuTrigger>
-                      <CircleEllipsis className="size-5 self-end text-[#808080]" />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="flex w-[80px] items-center justify-center">
-                      <DropdownMenuItem
-                        className="flex items-center justify-center"
-                        onSelect={() => onDelete({id: conn.id})}>
-                        <span className="text-center font-medium text-red-500">
-                          Delete
-                        </span>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </div>
+              <div className="relative flex size-full flex-col items-center justify-center gap-1">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="absolute right-0 top-0">
+                    <CircleEllipsis className="size-5 text-[#808080]" />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="flex w-[80px] items-center justify-center">
+                    <DropdownMenuItem
+                      className="flex items-center justify-center"
+                      onSelect={() => onDelete({id: conn.id})}>
+                      <span className="text-center font-medium text-red-500">
+                        Delete
+                      </span>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+
                 <ConnectorLogo
                   connector={conn.connectorConfig.connector}
                   className="size-[64px] rounded-lg"
@@ -72,22 +65,18 @@ export function ConnectionCard({
                     .toUpperCase() +
                     conn.connectorConfig.connector.displayName.slice(1)}
                 </p>
-                {conn.pipelineIds.length > 0 && (
-                  <div>
-                    {conn.syncInProgress ? (
-                      <div className="flex flex-row items-center justify-start gap-2">
-                        <Loader className="size-5 animate-spin text-button" />
-                        <p className="text-center text-sm text-button">
-                          Syncing...
-                        </p>
-                      </div>
-                    ) : (
-                      <p className="text-center text-sm text-[#808080]">
-                        Synced
+                <div>
+                  {conn.syncInProgress ? (
+                    <div className="flex flex-row items-center justify-start gap-2">
+                      <Loader className="size-5 animate-spin text-button" />
+                      <p className="text-center text-sm text-button">
+                        Syncing...
                       </p>
-                    )}
-                  </div>
-                )}
+                    </div>
+                  ) : (
+                    <p className="text-center text-sm text-[#808080]">Synced</p>
+                  )}
+                </div>
               </div>
             </CardContent>
           </Card>
