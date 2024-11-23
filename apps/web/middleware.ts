1ba1f44236736190612624b52cfb2404f189eb94
Tony <tony@openint.dev>
2024-11-22 19:43:25 -0800

    fix: Modify middleware to only dashboard route for clerk

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,13 +3,7 @@ import {clerkMiddleware} from '@clerk/nextjs/server'
 // Disable redirects
 export default clerkMiddleware()
 
+// Only want clerk to deal with dashboard routes
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Skip fothe connect route
-    '/((?!connect|api|debug).*)',
-    // Always run for API routes
-    '/(api|trpc|connector)(.*)',
-  ],
+  matcher: ['/dashboard/:path*', '/api/:path*'],
 }
