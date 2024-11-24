db62d95371b63f1dcdee457ae179aa7b329c3579
Amadeo Pellicce <pellicceama@gmail.com>
2024-11-23 23:49:31 -0800

    adding refresh token to proxy handler

--- packages/api/proxyHandler.ts
+++ packages/api/proxyHandler.ts
@@ -1,4 +1,4 @@
-import {getRemoteContext} from '@openint/cdk'
+import {extractId, getRemoteContext, initNangoSDK} from '@openint/cdk'
 import {getProtectedContext} from '@openint/trpc'
 import {contextFromRequest} from './createRouterHandler'
 
@@ -6,6 +6,45 @@ export const proxyHandler = async (req: Request) => {
   const ctx = await contextFromRequest({req})
   const protectedContext = getProtectedContext(ctx)
   const remoteContext = await getRemoteContext(protectedContext)
+
+  const credentialsExpired = remoteContext.remote.settings.oauth?.credentials
+    .expires_at
+    ? new Date(remoteContext.remote.settings.oauth.credentials.expires_at) <
+      new Date()
+    : false
+
+  if (credentialsExpired && remoteContext.remoteResourceId) {
+    const nango = initNangoSDK({
+      headers: {authorization: `Bearer ${process.env['NANGO_SECRET_KEY']}`},
+    })
+    const resourceExternalId = extractId(remoteContext.remoteResourceId)[2]
+    const nangoConnection = await nango
+      .GET('/connection/{connectionId}', {
+        params: {
+          path: {
+            connectionId: resourceExternalId,
+          },
+          query: {
+            provider_config_key: remoteContext.remote.connectorConfigId,
+            force_refresh: true,
+          },
+        },
+      })
+      .then((r) => r.data)
+
+    const {connectorConfig: int} =
+      await protectedContext.asOrgIfNeeded.getResourceExpandedOrFail(
+        remoteContext.remoteResourceId,
+      )
+
+    await protectedContext.asOrgIfNeeded._syncResourceUpdate(int, {
+      settings: {
+        oauth: nangoConnection,
+      },
+      resourceExternalId,
+    })
+  }
+
   const res = await remoteContext.remote.connector.proxy?.(
     remoteContext.remote.instance,
     req,
