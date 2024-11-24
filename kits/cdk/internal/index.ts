711e7ba610502e0d515d4d897f598555e726b894
Amadeo Pellicce <pellicceama@gmail.com>
2024-11-23 23:50:54 -0800

    readding missing export

--- kits/cdk/internal/index.ts
+++ kits/cdk/internal/index.ts
@@ -1,6 +1,7 @@
 // codegen:start {preset: barrel, include: "./{*.{ts,tsx},*/index.{ts,tsx}}", exclude: "./**/*.{spec,test,fixture}.{ts,tsx}"}
 export * from './api-key-encoding'
 export * from './NangoClient'
+export * from './nangoProxyLink'
 export * from './oauthConnector'
 export * from './remote-procedure'
 // codegen:end
