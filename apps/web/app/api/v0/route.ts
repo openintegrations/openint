5dbc766a0c3f2a64e1f87d0f3de938d78fb8071f
Amadeo Pellicce <pellicceama@gmail.com>
2024-11-26 16:58:02 -0800

    updating docs

--- apps/web/app/api/v0/route.ts
+++ apps/web/app/api/v0/route.ts
@@ -6,18 +6,19 @@ export function GET() {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Elements in HTML</title>
-      <!-- Embed elements Elements via Web Component -->
-      <script src="https://unpkg.com/@stoplight/elements/web-components.min.js"></script>
-      <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements/styles.min.css">
+      <title>Elements Dev Portal</title>
+  
+      <script src="https://unpkg.com/@stoplight/elements-dev-portal/web-components.min.js"></script>
+      <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements-dev-portal/styles.min.css">
     </head>
     <body>
 
-      <elements-api
-        apiDescriptionUrl="/api/v0/openapi.json"
+      <elements-stoplight-project
+        projectId="cHJqOjI2NTQzOA"
         router="hash"
+        apiDescriptionUrl="/api/v0/openapi.json"
         layout="sidebar"
-      />
+      ></elements-stoplight-project>
 
     </body>
   </html>
