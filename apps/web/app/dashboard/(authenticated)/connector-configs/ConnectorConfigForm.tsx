15191b59646a19e5aaf191cc1438bdadc915dbbf
Rodrigo Arze Leon <ral77ro@gmail.com>
2024-11-29 13:59:20 -0400

    Resolve re-renders in ccfgs sheet and handle row click on connected configs. (#102)
    * Move state to parent component and handle a single sheet
    
    * Move form to a separate component
    
    * Support row click
    
    ---------
    
    Co-authored-by: Rodrigo Arze Leon <rodrigo.arze@Rodrigos-Mac-mini.local>

---
+++ apps/web/app/dashboard/(authenticated)/connector-configs/ConnectorConfigForm.tsx
@@ -0,0 +1,176 @@
+'use client'
+
+import {Loader2} from 'lucide-react'
+import {zId, zRaw} from '@openint/cdk'
+import {_trpcReact} from '@openint/engine-frontend'
+import {SchemaForm, toast} from '@openint/ui'
+import type {ConnectorMeta, SchemaFormElement} from '@openint/ui'
+import {z} from '@openint/util'
+import {useCurrengOrg} from '@/components/viewer-context'
+import type {ConnectorConfig} from './ConnectorConfigPage'
+
+interface ConnectorConfigFormProps {
+  connectorName: string
+  ccfg?: Omit<ConnectorConfig, 'connectorName'>
+  connectorMeta: ConnectorMeta
+  setOpen: (open: boolean) => void
+  formRef: React.RefObject<SchemaFormElement>
+  isLoading: boolean
+}
+
+export function ConnectorConfigForm({
+  connectorName,
+  ccfg,
+  connectorMeta,
+  setOpen,
+  formRef,
+  isLoading,
+}: ConnectorConfigFormProps) {
+  const trpcUtils = _trpcReact.useContext()
+  const resourcesRes = _trpcReact.listResources.useQuery({
+    connectorName: 'postgres',
+  })
+
+  const zResoId = resourcesRes.data?.length
+    ? z.union(
+        resourcesRes.data.map((r) =>
+          z.literal(r.id).openapi({
+            title: r.displayName ? `${r.displayName} <${r.id}>` : r.id,
+          }),
+        ) as [z.ZodLiteral<string>, z.ZodLiteral<string>],
+      )
+    : zId('reso')
+
+  const ccfgSchema = (
+    connectorMeta?.schemas.connectorConfig
+      ? // Sometimes we have extra data inside the config due to extra data, so workaround for now
+
+        // as we have no way of displaying such information / allow user to fix it
+        {...connectorMeta?.schemas.connectorConfig, additionalProperties: true}
+      : undefined
+  ) as {type: 'object'; properties?: {}; additionalProperties: boolean}
+
+  // Consider calling this provider, actually seem to make more sense...
+  // given that we call the code itself connector config
+  const formSchema = zRaw.connector_config
+    .pick({displayName: true, disabled: true})
+    .extend({
+      config: z.object({}),
+      ...(connectorMeta?.supportedModes.includes('source') && {
+        defaultPipeOut: z
+          .union([
+            z.null().openapi({title: 'Disabled'}),
+            z
+              .object({
+                ...(connectorMeta?.sourceStreams?.length && {
+                  streams: z
+                    .object(
+                      Object.fromEntries(
+                        (connectorMeta.sourceStreams as [string]).map((s) => [
+                          s,
+                          z.boolean(),
+                        ]),
+                      ),
+                      // z.enum(connectorMeta.sourceStreams as [string]),
+                      // z.boolean(),
+                    )
+                    .passthrough()
+                    .openapi({description: 'Entities to sync'}),
+                }),
+                links: zRaw.connector_config.shape.defaultPipeOut
+                  .unwrap()
+                  .unwrap().shape.links,
+                destination_id: zResoId.optional().openapi({
+                  description: 'Defaults to the org-wide postgres',
+                }),
+              })
+              .openapi({title: 'Enabled'}),
+          ])
+          .openapi({
+            title: 'Sync settings',
+            description: zRaw.connector_config.shape.defaultPipeOut.description,
+          }),
+      }),
+      ...(connectorMeta?.supportedModes.includes('destination') && {
+        defaultPipeIn: z
+          .union([
+            z.null().openapi({title: 'Disabled'}),
+            z
+              .object({
+                links: zRaw.connector_config.shape.defaultPipeIn
+                  .unwrap()
+                  .unwrap().shape.links,
+                source_id: zResoId,
+              })
+              .openapi({title: 'Enabled'}),
+          ])
+          .openapi({
+            title: 'Reverse sync settings',
+            description: zRaw.connector_config.shape.defaultPipeIn.description,
+          }),
+      }),
+    })
+  connectorMeta?.__typename
+
+  const {orgId} = useCurrengOrg()
+
+  const upsertConnectorConfig =
+    _trpcReact.adminUpsertConnectorConfig.useMutation({
+      onSuccess: () => {
+        setOpen(false)
+        toast({title: 'connector config saved', variant: 'success'})
+        void trpcUtils.adminListConnectorConfigs.invalidate()
+        void trpcUtils.listConnectorConfigInfos.invalidate()
+      },
+      onError: (err) => {
+        toast({
+          title: 'Failed to save connector config',
+          description: `${err}`,
+          variant: 'destructive',
+        })
+      },
+    })
+
+  return (
+    <div className="grow overflow-scroll">
+      {isLoading ? (
+        <Loader2 className="size-6 animate-spin" />
+      ) : (
+        <SchemaForm
+          ref={formRef}
+          schema={formSchema}
+          jsonSchemaTransform={(schema) => ({
+            ...schema,
+            properties: {
+              ...schema.properties,
+              ...(ccfgSchema && {config: ccfgSchema}),
+            },
+          })}
+          formData={
+            ccfg
+              ? {
+                  displayName: ccfg.displayName,
+                  disabled: ccfg.disabled,
+                  config: ccfg.config ?? {},
+                  defaultPipeOut: ccfg.defaultPipeOut ?? null,
+                  defaultPipeIn: ccfg.defaultPipeIn ?? null,
+                } // {} because required
+              : undefined
+          }
+          // formData should be non-null at this point, we should fix the typing
+          loading={upsertConnectorConfig.isLoading}
+          onSubmit={({formData}) => {
+            console.log('formData submitted', formData)
+            upsertConnectorConfig.mutate({
+              ...formData,
+              ...(ccfg ? {id: ccfg.id} : {connectorName}),
+              orgId,
+            })
+          }}
+          hideSubmitButton
+        />
+      )}
+      {!ccfgSchema && <p>No configuration needed</p>}
+    </div>
+  )
+}
--- apps/web/app/dashboard/(authenticated)/connector-configs/ConnectorConfigPage.tsx
+++ apps/web/app/dashboard/(authenticated)/connector-configs/ConnectorConfigPage.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import {Loader2, Lock} from 'lucide-react'
+import {Loader2, Lock, Pencil, Plus} from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, {useState} from 'react'
 import {zConnectorStage, zVerticalKey} from '@openint/cdk'
 import type {RouterOutput} from '@openint/engine-backend'
 import {_trpcReact} from '@openint/engine-frontend'
@@ -11,10 +11,10 @@ import {
   ConnectorCard as _ConnectorCard,
   ConnectorConfigCard as _ConnectorConfigCard,
   Badge,
+  Button,
   cn,
   ConnectorLogo,
   DataTable,
-  LoadingText,
   parseCategory,
 } from '@openint/ui'
 import {inPlaceSort, R, titleCase} from '@openint/util'
@@ -27,10 +27,20 @@ export default function ConnectorConfigsPage({
 }: {
   showCTAs?: boolean
 }) {
+  const [open, setOpen] = useState(false)
+  const [connectorName, setConnectorName] = useState<string>('')
+  const [connectorConfig, setConnectorConfig] = useState<
+    Omit<ConnectorConfig, 'connectorName'> | undefined
+  >(undefined)
+
   const connectorConfigsRes = _trpcReact.adminListConnectorConfigs.useQuery()
   const catalog = _trpcReact.listConnectorMetas.useQuery()
   if (!connectorConfigsRes.data || !catalog.data) {
-    return <LoadingText className="block p-4" />
+    return (
+      <div className="flex size-full items-center justify-center">
+        <Loader2 className="size-8 animate-spin text-button" />
+      </div>
+    )
   }
 
   const filter = (c: ConnectorConfig) =>
@@ -48,6 +58,11 @@ export default function ConnectorConfigsPage({
         <DataTable
           query={connectorConfigsRes}
           filter={filter}
+          onRowClick={(row) => {
+            setConnectorName(row.connectorName)
+            setConnectorConfig(row)
+            setOpen(true)
+          }}
           columns={[
             {
               id: 'connectorName',
@@ -107,10 +122,17 @@ export default function ConnectorConfigsPage({
                 const connector = catalog.data[row.original.connectorName]
                 if (!connector) return null
                 return (
-                  <ConnectorConfigSheet
-                    connectorConfig={row.original}
-                    connectorName={connector.name}
-                  />
+                  <Button
+                    variant="secondary"
+                    className="size-sm flex items-center gap-2"
+                    onClick={() => {
+                      setConnectorName(row.original.connectorName)
+                      setConnectorConfig(row.original)
+                      setOpen(true)
+                    }}>
+                    <Pencil className="h-5 w-5 text-black" />
+                    <span className="text-black">Edit</span>
+                  </Button>
                 )
               },
             },
@@ -160,7 +182,18 @@ export default function ConnectorConfigsPage({
                         <p className="text-sm font-semibold">Request access</p>
                       </div>
                     ) : (
-                      <ConnectorConfigSheet connectorName={connector.name} />
+                      <div
+                        className={cn(
+                          'flex size-full cursor-pointer flex-col items-center justify-center gap-2 text-button',
+                        )}
+                        onClick={() => {
+                          setConnectorName(connector.name)
+                          setConnectorConfig(undefined)
+                          setOpen(true)
+                        }}>
+                        <Plus />
+                        <p className="text-sm font-semibold">Add</p>
+                      </div>
                     ))}
                 </ConnectorCard>
               ))}
@@ -168,6 +201,12 @@ export default function ConnectorConfigsPage({
           </div>
         )
       })}
+      <ConnectorConfigSheet
+        connectorName={connectorName}
+        connectorConfig={connectorConfig}
+        open={open}
+        setOpen={setOpen}
+      />
     </div>
   )
 }
