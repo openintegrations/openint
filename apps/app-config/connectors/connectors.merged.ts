2316abcaa49f6f1c63a7fa260bda90295af8816e
Amadeo Pellicce <pellicceama@gmail.com>
2024-12-04 20:51:15 -0800

    nesting outlook under ms

--- apps/app-config/connectors/connectors.merged.ts
+++ apps/app-config/connectors/connectors.merged.ts
@@ -44,8 +44,6 @@ import {default as connectorMoota_def} from '@openint/connector-moota/def'
 import {default as connectorMoota_server} from '@openint/connector-moota/server'
 import {default as connectorOnebrick_def} from '@openint/connector-onebrick/def'
 import {default as connectorOnebrick_server} from '@openint/connector-onebrick/server'
-import {default as connectorOutlook_def} from '@openint/connector-outlook/def'
-import {default as connectorOutlook_server} from '@openint/connector-outlook/server'
 import {default as connectorOutreach_def} from '@openint/connector-outreach/def'
 import {default as connectorOutreach_server} from '@openint/connector-outreach/server'
 import {default as connectorPipedrive_def} from '@openint/connector-pipedrive/def'
@@ -204,11 +202,6 @@ const connectorOnebrick = {
   ...connectorOnebrick_server,
 }
 
-const connectorOutlook = {
-  ...connectorOutlook_def,
-  ...connectorOutlook_server,
-}
-
 const connectorOutreach = {
   ...connectorOutreach_def,
   ...connectorOutreach_server,
@@ -342,7 +335,6 @@ export const mergedConnectors = {
   mongodb: connectorMongodb,
   moota: connectorMoota,
   onebrick: connectorOnebrick,
-  outlook: connectorOutlook,
   outreach: connectorOutreach,
   pipedrive: connectorPipedrive,
   plaid: connectorPlaid,
