a86b9ed8dd25aa68c17f02079852124f7c736dfc
Amadeo Pellicce <pellicceama@gmail.com>
2024-12-12 16:16:55 -0800

    adding alias for critters

--- apps/web/app/api/v0/[...trpc]/route.ts
+++ apps/web/app/api/v0/[...trpc]/route.ts
@@ -7,7 +7,7 @@ export const maxDuration = 300
 // TODO: Add handling for CORS
 // Also we may need to check for req.headers['transfer-encoding'] === 'chunked'
 // Was not supported on pages router, maybe better on app router?
-const handler = withClerkMiddleware(createAppHandler({endpoint: '/api/v0'}))
+const handler = createAppHandler({endpoint: '/api/v0'});
 
 export {
   handler as DELETE,
