5def7d58869cd5594153156bf579fda718962424
Amadeo Pellicce <pellicceama@gmail.com>
2024-12-13 14:24:30 -0800

    tweaking config

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -1,5 +1,13 @@
 export default {
-  default: {},
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
   middleware: {
     external: true,
     override: {
