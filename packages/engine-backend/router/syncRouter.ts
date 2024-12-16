6dcd9881b37a6c60ab9307626d18237adcdd1c50
Amadeo Pellicce <pellicceama@gmail.com>
2024-12-16 15:00:42 -0800

    removing unecessasry ts ignores

--- packages/engine-backend/router/syncRouter.ts
+++ packages/engine-backend/router/syncRouter.ts
@@ -14,14 +14,14 @@ export const syncRouter = trpc.router({
     .input(zListParams.optional())
     .output(z.array(z.unknown()))
     .query(async ({ctx}) => {
-      const connections = await ctx.services.metaService.tables.connection.list({})
+      const connections = await ctx.services.metaService.tables.connection.list(
+        {},
+      )
       if (connections.length === 0) {
         return []
       }
       const runs = await configDb.query.sync_run.findMany({
-        // @ts-expect-error
         where: inArray(
-          // @ts-expect-error
           schema.sync_run.connection_id,
           connections.map((r) => r.id),
         ),
