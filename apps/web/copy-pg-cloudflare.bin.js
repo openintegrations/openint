1aaf771b124a04392900b3a33a199cbc9938ab4c
Amadeo Pellicce <pellicceama@gmail.com>
2024-12-17 18:18:02 -0800

    script to patch deps

---
+++ apps/web/copy-pg-cloudflare.bin.js
@@ -0,0 +1,33 @@
+// Required for build:worker
+const path = require('path')
+const {execSync} = require('child_process')
+
+const fs = require('fs')
+
+const sourcePath = path.resolve(
+  __dirname,
+  '../../node_modules/.pnpm/pg@8.13.1_pg-native@3.0.1/node_modules/pg-cloudflare/dist/index.js',
+)
+const destinationPath = path.resolve(
+  __dirname,
+  '.next/standalone/node_modules/.pnpm/pg@8.13.1_pg-native@3.0.1/node_modules/pg-cloudflare/dist',
+)
+
+if (!fs.existsSync(sourcePath)) {
+  console.error(`PG Cloudflare file does not exist: ${sourcePath}`)
+  process.exit(1)
+}
+
+if (!fs.existsSync(destinationPath)) {
+  console.error(
+    `PG Cloudflare destination directory does not exist: ${destinationPath}`,
+  )
+  process.exit(1)
+}
+
+try {
+  execSync(`copy "${sourcePath}" "${destinationPath}"`, {stdio: 'inherit'})
+} catch (error) {
+  console.error('Error copying file:', error)
+  process.exit(1)
+}
