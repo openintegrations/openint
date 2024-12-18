e7bac5df1642cd6061d62fb10989552cdf2bd3ed
Amadeo Pellicce <pellicceama@gmail.com>
2024-12-18 13:01:16 -0800

    not setting domain in non CF worker deploys

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,10 +1,17 @@
 import {clerkMiddleware, createClerkClient} from '@clerk/nextjs/server'
 
 const client = createClerkClient({secretKey: process.env['CLERK_SECRET_KEY']})
+
+function getRuntimeClerkDomain() {
+  if (process.env['NEXT_PUBLIC_RUNTIME_ENV'] === 'edge') {
+    return process.env['NEXT_PUBLIC_SERVER_URL']
+  }
+  return undefined
+}
 // Disable redirects
 export default clerkMiddleware({
   publishableKey: process.env['NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY'],
-  domain: process.env['NEXT_PUBLIC_SERVER_URL'],
+  domain: getRuntimeClerkDomain(),
   apiClient: client,
 })
 
