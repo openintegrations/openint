5ad73df6ae984eb35460cf28a81df5d730dfaa1b
Rodrigo Arze Leon <rodrigo.arze@Rodrigos-Mac-mini.local>
2025-01-03 12:03:31 -0400

    use openint sdk

--- packages/engine-backend/router/__tests__/connectionRouter.spec.ts
+++ packages/engine-backend/router/__tests__/connectionRouter.spec.ts
@@ -7,46 +7,50 @@ describe('connectionRouter', () => {
     jest.setTimeout(10000)
 
     it('should list connections', async () => {
-      const clerk = createClerkClient({
-        secretKey: process.env.CLERK_SECRET_KEY as string,
-      })
-
-      // Create test user with required fields
-      const testUser = await clerk.users.createUser({
-        emailAddress: ['test@example.com'],
-        firstName: 'Test',
-        lastName: 'User',
-        password: 'test-password-123',
-      })
-
-      console.log('testUser:', testUser)
-
-      // Create a session token
-      const signInToken = await clerk.signInTokens.createSignInToken({
-        userId: testUser.id,
-        expiresInSeconds: 300, // 5 minutes
-      })
-
-      console.log('signInToken:', signInToken)
-
-      // const openint = initOpenIntSDK({
-      //   baseUrl: 'http://localhost:4000/api/v0',
-      //   headers: {
-      //     'x-apikey': process.env.OPENINT_API_KEY ?? '',
-      //     Authorization: `Bearer ${signInToken.token}`,
-      //   },
+      // const clerk = createClerkClient({
+      //   secretKey: process.env.CLERK_SECRET_KEY as string,
       // })
 
-      // try {
-      //   const response = await openint.GET('/core/connection')
-      //   console.log('response:', response)
-      // } catch (err) {
-      //   console.error('Error:', err)
-      //   throw err
-      // } finally {
-      //   // Cleanup
-      //   await clerk.users.deleteUser(testUser.id)
-      // }
+      // // Create test user with required fields
+      // const testUser = await clerk.users.createUser({
+      //   emailAddress: ['test@example.com'],
+      //   firstName: 'Test',
+      //   lastName: 'User',
+      //   password: 'test-password-123',
+      // })
+
+      // console.log('testUser:', testUser)
+
+      // // Create a session token
+      // const signInToken = await clerk.signInTokens.createSignInToken({
+      //   userId: testUser.id,
+      //   expiresInSeconds: 300, // 5 minutes
+      // })
+
+      // const session = await clerk.sessions.getSession('sess_123')
+
+      // console.log('signInToken:', signInToken)
+
+      const openint = initOpenIntSDK({
+        baseUrl: 'http://localhost:4000/api/v0',
+        headers: {
+          'x-apikey': process.env.OPENINT_API_KEY ?? '',
+          // Authorization: `Bearer ${signInToken.token}`,
+        },
+      })
+
+      console.log('openint:', openint)
+
+      try {
+        const response = await openint.GET('/core/connection')
+        console.log('response:', response)
+      } catch (err) {
+        console.error('Error:', err)
+        throw err
+      } finally {
+        // Cleanup
+        // await clerk.users.deleteUser(testUser.id)
+      }
     })
   })
 })
