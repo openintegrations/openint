aa801701b9b8ad7406e3a9aa645454f3a8dfd2bb
Rodrigo Arze Leon <rodrigo.arze@Rodrigos-Mac-mini.local>
2025-01-03 19:37:38 -0400

    Add some basic workings tests

--- packages/engine-backend/router/__tests__/connectionRouter.spec.ts
+++ packages/engine-backend/router/__tests__/connectionRouter.spec.ts
@@ -1,56 +1,83 @@
-import {createClerkClient} from '@clerk/backend'
 import {jest} from '@jest/globals'
-import {initOpenIntSDK} from '@openint/sdk'
+import {initOpenIntSDK, type OpenIntSDK} from '@openint/sdk'
 
 describe('connectionRouter', () => {
   describe('listConnection', () => {
-    jest.setTimeout(10000)
+    jest.setTimeout(20000)
+    let openint: OpenIntSDK
+    let ccfgId: string
+    // let connectionId: string
 
-    it('should list connections', async () => {
-      // const clerk = createClerkClient({
-      //   secretKey: process.env.CLERK_SECRET_KEY as string,
-      // })
-
-      // // Create test user with required fields
-      // const testUser = await clerk.users.createUser({
-      //   emailAddress: ['test@example.com'],
-      //   firstName: 'Test',
-      //   lastName: 'User',
-      //   password: 'test-password-123',
-      // })
-
-      // console.log('testUser:', testUser)
-
-      // // Create a session token
-      // const signInToken = await clerk.signInTokens.createSignInToken({
-      //   userId: testUser.id,
-      //   expiresInSeconds: 300, // 5 minutes
-      // })
-
-      // const session = await clerk.sessions.getSession('sess_123')
-
-      // console.log('signInToken:', signInToken)
-
-      const openint = initOpenIntSDK({
+    beforeAll(async () => {
+      openint = initOpenIntSDK({
         baseUrl: 'http://localhost:4000/api/v0',
         headers: {
           'x-apikey': process.env.OPENINT_API_KEY ?? '',
-          // Authorization: `Bearer ${signInToken.token}`,
         },
       })
+    })
 
-      console.log('openint:', openint)
-
+    it('should list connections', async () => {
       try {
         const response = await openint.GET('/core/connection')
-        console.log('response:', response)
+        expect(response.response.status).toBe(200)
+        expect(response.data.length).toBe(0)
       } catch (err) {
         console.error('Error:', err)
         throw err
-      } finally {
-        // Cleanup
-        // await clerk.users.deleteUser(testUser.id)
       }
     })
+
+    it('should create a connector config', async () => {
+      try {
+        const response = await openint.POST('/core/connector_config', {
+          body: {
+            config: {},
+            defaultPipeOut: {},
+            connectorName: 'greenhouse',
+            orgId: 'org_2r7t02yvrfEhad6NtXaJdUcgIyc',
+          },
+        })
+        ccfgId = response.data.id
+        expect(response.response.status).toBe(200)
+        expect(response.data.id).toBeDefined()
+      } catch (err) {
+        console.error('Error:', err)
+        throw err
+      }
+    })
+
+    it('should create a connection', async () => {
+      const response = await openint.POST('/core/connection', {
+        body: {
+          connectorConfigId: ccfgId,
+          settings: {
+            apiKey: 'e5a6a86e28e17bd5d17307e6fcbb3683-8',
+          },
+          displayName: 'Greenhouse Test',
+          disabled: false,
+          customerId: 'cust_test',
+          metadata: {},
+          integrationId: 'int_test',
+        },
+      })
+
+      console.log('response:', response)
+
+      expect(response.response.status).toBe(200)
+    })
+
+    // it('should delete a connector config', async () => {
+    //   const options = {
+    //     method: 'DELETE',
+    //     headers: {'x-apikey': '<api-key>', 'x-connection-id': '<api-key>'},
+    //   }
+
+    //   const response = await openint.DELETE(
+    //     `/core/connector_config/${ccfgId}` as '/core/connector_config/{id}',
+    //     {params: {path: {id: ccfgId}}, ...options},
+    //   )
+    //   expect(response.response.status).toBe(200)
+    // })
   })
 })
