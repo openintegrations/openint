5ef7a51691387dcf3e135c9585b23581b1bac434
santiago <156690976+snrondina@users.noreply.github.com>
2025-01-13 19:30:20 -0300

    settings button fix (#222)

--- apps/web/app/dashboard/(authenticated)/settings/page.tsx
+++ apps/web/app/dashboard/(authenticated)/settings/page.tsx
@@ -5,7 +5,7 @@ import type {AppRouter} from '@openint/api'
 import {zOrganization} from '@openint/engine-backend/services/AuthProvider'
 import type {TRPCReact} from '@openint/engine-frontend'
 import {_trpcReact, useMutationToast} from '@openint/engine-frontend'
-import {SchemaForm} from '@openint/ui'
+import {Button, SchemaForm} from '@openint/ui'
 import useRefetchOnSwitch from '../useRefetchOnSwitch'
 
 const trpcReact = _trpcReact as unknown as TRPCReact<AppRouter>
@@ -54,7 +54,11 @@ export default function SettingsPage() {
             },
           )
         }}
-      />
+      >
+        <Button type="submit" className="mt-4">
+          Save Settings
+        </Button>
+      </SchemaForm>
     </div>
   ) : (
     <div className="flex size-full flex-1 items-center justify-center">
