1fff862bdbcbbd53f6eb3f95b7a3abe031ec7c9a
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2025-01-20 04:07:30 +0100

    new unified file storage API (#227)
    * test: plaid adapter test
    
    * Get test to use procedure definitions
    
    * Change to 4.x version of change-case that is not esm only so it works in jest
    
    * Router based test, test if implemented also
    
    * abstract out forEachAdapterConnections
    
    * Move vertical-test-utils into vdk
    
    * file storage test
    
    * Add basic test for unified file storage
    
    * Remove plaid adapter test
    
    * minor cleanup
    
    * Clean up deps
    
    * comments
    
    * rename unified-etl to unified-sync
    
    * lints
    
    * doc typo
    
    * updating docs
    
    * adding whitelist to docs
    
    * updating docs
    
    * new unified file storage API
    
    * migrating ms adapter to sharepoint and implementing new api
    
    ---------
    
    Co-authored-by: OpenInt Bot <team@openint.dev>

--- unified/unified-file-storage/router.ts
+++ unified/unified-file-storage/router.ts
@@ -10,8 +10,6 @@ import {
 import adapters from './adapters'
 import * as unified from './unifiedModels'
 
-export {unified}
-
 function oapi(meta: NonNullable<RouterMeta['openapi']>): RouterMeta {
   const path = `/unified/file-storage${meta.path}` satisfies `/${string}`
   return {openapi: {...meta, path, tags: ['File Storage']}}
@@ -20,35 +18,70 @@ function oapi(meta: NonNullable<RouterMeta['openapi']>): RouterMeta {
 const procedure = verticalProcedure(adapters)
 
 export const fileStorageRouter = trpc.router({
-  listDrives: procedure
-    .meta(oapi({method: 'GET', path: '/drive'}))
+  listDriveGroups: procedure
+    .meta(oapi({method: 'GET', path: '/drive-groups'}))
     .input(zPaginationParams.nullish())
+    .output(zPaginatedResult.extend({items: z.array(unified.DriveGroup)}))
+    .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
+
+  listDrives: procedure
+    .meta(oapi({method: 'GET', path: '/drives'}))
+    .input(
+      zPaginationParams
+        .extend({
+          driveGroupId: z.string().optional(),
+        })
+        .nullish(),
+    )
     .output(zPaginatedResult.extend({items: z.array(unified.Drive)}))
     .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
-  getDrive: procedure
-    .meta(oapi({method: 'GET', path: '/drive/{driveId}'}))
-    .input(z.object({driveId: z.string()}))
-    .output(unified.Drive)
-    .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
-  listFolders: procedure
-    .meta(oapi({method: 'GET', path: '/drive/{driveId}/folder'}))
-    .input(zPaginationParams.extend({driveId: z.string()}))
-    .output(zPaginatedResult.extend({items: z.array(unified.Folder)}))
-    .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
-  getFolder: procedure
-    .meta(oapi({method: 'GET', path: '/drive/{driveId}/folder/{folderId}'}))
-    .input(z.object({driveId: z.string(), folderId: z.string()}))
-    .output(unified.Folder)
-    .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
+
   listFiles: procedure
-    .meta(oapi({method: 'GET', path: '/drive/{driveId}/file'}))
-    .input(zPaginationParams.extend({driveId: z.string(), folderId: z.string().nullish()}))
+    .meta(oapi({method: 'GET', path: '/files'}))
+    .input(
+      zPaginationParams
+        .extend({
+          driveId: z.string().optional(),
+          folderId: z.string().optional(),
+        })
+        .nullish(),
+    )
     .output(zPaginatedResult.extend({items: z.array(unified.File)}))
     .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
+
   getFile: procedure
-    .meta(oapi({method: 'GET', path: '/drive/{driveId}/file/{fileId}'}))
-    .input(z.object({driveId: z.string(), fileId: z.string()}))
+    .meta(oapi({method: 'GET', path: '/files/{id}'}))
+    .input(z.object({id: z.string()}))
     .output(unified.File)
+    .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
+
+  exportFile: procedure
+    .meta(oapi({method: 'GET', path: '/files/{id}/export'}))
+    .input(
+      z.object({
+        id: z.string(),
+        format: z.string(),
+      }),
+    )
+    .output(z.instanceof(ReadableStream))
+    .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
+
+  downloadFile: procedure
+    .meta(oapi({method: 'GET', path: '/files/{id}/download'}))
+    .input(z.object({id: z.string()}))
+    .output(z.instanceof(ReadableStream))
+    .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
+
+  listFolders: procedure
+    .meta(oapi({method: 'GET', path: '/folders'}))
+    .input(
+      zPaginationParams
+        .extend({
+          driveId: z.string().optional(),
+        })
+        .nullish(),
+    )
+    .output(zPaginatedResult.extend({items: z.array(unified.Folder)}))
     .query(async ({input, ctx}) => proxyCallAdapter({input, ctx})),
 })
 
