edadfe9e988a183418178824be93ac9b05dcce7d
Amadeo Pellicce <pellicceama@gmail.com>
2025-01-25 17:05:18 -0800

    lint

--- jest.config.js
+++ jest.config.js
@@ -31,3 +31,11 @@ module.exports = {
     'jest-watch-typeahead/testname',
   ],
 }
+
+// NOTE maybe required for kits/file-picker
+// import '@testing-library/jest-dom';
+
+// If you are using @jest/globals with injectGlobals: false, you will need to use a different import in your tests setup file:
+
+// // In your own jest-setup.js (or any other name)
+// import '@testing-library/jest-dom/jest-globals'
--- kits/file-picker/jest.setup.js
+++
@@ -1,4 +0,0 @@
-import '@testing-library/jest-dom';
-// If you are using @jest/globals with injectGlobals: false, you will need to use a different import in your tests setup file:
-// // In your own jest-setup.js (or any other name)
-// import '@testing-library/jest-dom/jest-globals'
