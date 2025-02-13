d00778f2571b33d165d8607b114582bb398c615b
Rodrigo Arze Leon <rodrigo.arze@Rodrigos-Mac-mini.local>
2025-02-12 20:57:53 -0400

    Add hover state similar to integration card instead of icon

--- packages/ui/domain-components/ConnectionCard.tsx
+++ packages/ui/domain-components/ConnectionCard.tsx
@@ -1,4 +1,5 @@
 import {Loader2, Settings} from 'lucide-react'
+import {useState} from 'react'
 import type {ConnectorConfig} from '../../engine-frontend/hocs/WithConnectConfig'
 import {Card, CardContent} from '../shadcn'
 import {IntegrationLogo} from './ConnectionRawCard'
@@ -21,13 +22,17 @@ export function ConnectionCard({
     statusMessage: string
   }
 }) {
+  const [isHovered, setIsHovered] = useState(false)
   let connectionName =
     conn?.integration?.name ?? conn.connectorConfig.connector.displayName
   connectionName =
     connectionName.charAt(0).toUpperCase() + connectionName.slice(1)
 
   return (
-    <Card className="border-card-border relative h-[150px] w-[150px] cursor-pointer rounded-lg border bg-card p-0">
+    <Card
+      className="border-card-border relative h-[150px] w-[150px] cursor-pointer rounded-lg border bg-card p-0 transition-colors duration-300 ease-in-out hover:border-button hover:bg-button-light"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
       {/* Overlay spinner */}
       {reconnectId && (
         <div className="absolute inset-0 z-10 flex items-center justify-center rounded-md bg-card-loading">
@@ -39,46 +44,55 @@ export function ConnectionCard({
         className="flex h-full flex-col items-center justify-center p-4 py-2"
         onClick={onSelect}>
         <div className="relative flex size-full flex-col items-center justify-center gap-1">
-          <div className="absolute right-0 top-0">
-            <Settings className="size-5 text-foreground" onClick={onSelect} />
-          </div>
-
-          {conn?.integration?.logo_url || conn?.integration?.logoUrl ? (
-            <IntegrationLogo
-              integration={conn.integration}
-              className="size-[64px] rounded-lg"
-            />
+          {isHovered ? (
+            <div className="flex h-full flex-col items-center justify-center">
+              <Settings className="text-button" size={24} />
+              <span className="mt-2 font-sans text-[14px] font-semibold text-button">
+                Manage
+              </span>
+            </div>
           ) : (
-            <ConnectorLogo
-              connector={conn.connectorConfig.connector}
-              className="size-[64px] rounded-lg"
-            />
-          )}
-          <p
-            className={`m-0 max-w-[100px] text-center text-sm font-semibold ${
-              conn.connectorConfig.connector.displayName.length > 15
-                ? 'truncate'
-                : ''
-            }`}>
-            {connectionName}
-          </p>
-          <div>
-            {conn.status !== 'healthy' ? (
+            <>
+              {conn?.integration?.logo_url || conn?.integration?.logoUrl ? (
+                <IntegrationLogo
+                  integration={conn.integration}
+                  className="size-[64px] rounded-lg"
+                />
+              ) : (
+                <ConnectorLogo
+                  connector={conn.connectorConfig.connector}
+                  className="size-[64px] rounded-lg"
+                />
+              )}
               <p
-                className={`text-center text-sm ${
-                  reconnectId ? 'text-button' : 'text-red-500'
+                className={`m-0 max-w-[100px] text-center text-sm font-semibold ${
+                  conn.connectorConfig.connector.displayName.length > 15
+                    ? 'truncate'
+                    : ''
                 }`}>
-                {reconnectId ? 'Processing...' : 'Reconnect Required'}
+                {connectionName}
               </p>
-            ) : conn.syncInProgress ? (
-              <div className="flex flex-row items-center justify-start gap-2">
-                <Loader2 className="size-5 animate-spin text-button" />
-                <p className="text-center text-sm text-button">Syncing...</p>
+              <div>
+                {conn.status !== 'healthy' ? (
+                  <p
+                    className={`text-center text-sm ${
+                      reconnectId ? 'text-button' : 'text-red-500'
+                    }`}>
+                    {reconnectId ? 'Processing...' : 'Reconnect Required'}
+                  </p>
+                ) : conn.syncInProgress ? (
+                  <div className="flex flex-row items-center justify-start gap-2">
+                    <Loader2 className="size-5 animate-spin text-button" />
+                    <p className="text-center text-sm text-button">
+                      Syncing...
+                    </p>
+                  </div>
+                ) : (
+                  <p className="text-center text-sm text-[#808080]">Synced</p>
+                )}
               </div>
-            ) : (
-              <p className="text-center text-sm text-[#808080]">Synced</p>
-            )}
-          </div>
+            </>
+          )}
         </div>
       </CardContent>
     </Card>
