6e56acbef9c40490846ee861de593ccb9596e953
Rodrigo Arze Leon <ral77ro@gmail.com>
2025-02-14 09:44:45 -0400

    Remove syncInProgress, simplify sidebar, fix form paddings and set default values for connect dropdowns (#263)
    * Remove syncInProgress flag from backend and ui
    
    * Simplify sidebar and remove unused pages
    
    * Remove spacing in schema form
    
    * Set default values for connect deeplink dropdowns
    
    * Removing uncessary settings
    
    * tweaking sidebar
    
    * tsignore
    
    * lint
    
    * adding missing orgs
    
    ---------
    
    Co-authored-by: Rodrigo Arze Leon <rodrigo.arze@Rodrigos-Mac-mini.local>
    Co-authored-by: Amadeo Pellicce <pellicceama@gmail.com>

--- apps/web/vcommands/vcommand-definitions.ts
+++ apps/web/vcommands/vcommand-definitions.ts
@@ -1,19 +1,19 @@
 // import {useTheme} from 'next-themes';
-import type {CommandDefinitionInput, CommandDefinitionMap} from '@openint/ui';
-import {cmdInit} from '@openint/ui';
-import {z} from '@openint/util';
+import type {CommandDefinitionInput, CommandDefinitionMap} from '@openint/ui'
+import {cmdInit} from '@openint/ui'
+import {z} from '@openint/util'
 // import {delay} from '@openint/util';
-import {__DEBUG__} from '@/../app-config/constants';
-import {zClient} from '@/lib-common/schemas';
-import {copyToClipboard} from '../lib-client/copyToClipboard';
-import type {CommandContext} from './vcommand-context';
+import {__DEBUG__} from '@/../app-config/constants'
+import {zClient} from '@/lib-common/schemas'
+import {copyToClipboard} from '../lib-client/copyToClipboard'
+import type {CommandContext} from './vcommand-context'
 
-const cmd = cmdInit<CommandContext>();
+const cmd = cmdInit<CommandContext>()
 
 const _pipelineCommand = {
   group: 'pipeline',
-  params: z.object({ pipeline: zClient.pipeline }),
-} satisfies CommandDefinitionInput<CommandContext>;
+  params: z.object({pipeline: zClient.pipeline}),
+} satisfies CommandDefinitionInput<CommandContext>
 
 export const pipelineCommands = {
   // 'pipeline:create': cmd.identity({
@@ -26,23 +26,23 @@ export const pipelineCommands = {
   'pipeline:edit': cmd.identity({
     ..._pipelineCommand,
     icon: 'Pencil',
-    execute: ({ params: { pipeline }, ctx }) => {
-      ctx.setPipelineSheetState({ pipeline, open: true });
+    execute: ({params: {pipeline}, ctx}) => {
+      ctx.setPipelineSheetState({pipeline, open: true})
     },
   }),
   'pipeline:sync': cmd.identity({
     ..._pipelineCommand,
     icon: 'RefreshCw',
-    execute: ({ params: { pipeline }, ctx }) => {
+    execute: ({params: {pipeline}, ctx}) => {
       void ctx.withToast(() =>
-        ctx.trpcCtx.client.syncPipeline.mutate({ id: pipeline.id })
-      );
+        ctx.trpcCtx.client.syncPipeline.mutate({id: pipeline.id}),
+      )
     },
   }),
   'pipeline:delete': cmd.identity({
     icon: 'Trash',
     ..._pipelineCommand,
-    execute: ({ ctx, params }) =>
+    execute: ({ctx, params}) =>
       ctx.setAlertDialogState({
         title: `Confirm deleting pipeline ${params.pipeline.id}`,
         destructive: true,
@@ -50,91 +50,93 @@ export const pipelineCommands = {
         description:
           'Already synchronized data will be untouched. However this will delete any incremental sync state so when a new pipeline is created you will have to sync from scratch.',
         onConfirm: () =>
-          ctx.trpcCtx.client.deletePipeline.mutate({ id: params.pipeline.id }),
+          ctx.trpcCtx.client.deletePipeline.mutate({id: params.pipeline.id}),
       }),
   }),
-} satisfies CommandDefinitionMap<CommandContext>;
+} satisfies CommandDefinitionMap<CommandContext>
 
 const _connectionCommand = {
   group: 'connection',
-  params: z.object({ connection: zClient.connection }),
-} satisfies CommandDefinitionInput<CommandContext>;
+  params: z.object({connection: zClient.connection}),
+} satisfies CommandDefinitionInput<CommandContext>
 
 const debugConnectionCommands = {
   'connection:edit': cmd.identity({
     ..._connectionCommand,
     icon: 'Pencil',
     title: 'Edit Connection',
-    execute: ({ ctx, params }) =>
-      ctx.setConnectionSheetState({ connection: params.connection, open: true }),
+    execute: ({ctx, params}) =>
+      ctx.setConnectionSheetState({connection: params.connection, open: true}),
   }),
   'connection:navigate_sql': cmd.identity({
     ..._connectionCommand,
     icon: 'Database',
     title: 'Run sql',
-    execute: ({ params: { connection }, ctx }) => {
-      ctx.router.push(`/dashboard/connections/${connection.id}/sql`);
+    execute: ({params: {connection}, ctx}) => {
+      ctx.router.push(`/dashboard/connections/${connection.id}/sql`)
     },
   }),
   'connection:navigate_playground': cmd.identity({
     ..._connectionCommand,
     icon: 'Database',
     title: 'Playground',
-    execute: ({ params: { connection }, ctx }) => {
-      ctx.router.push(`/dashboard/connections/${connection.id}/playground`);
+    execute: ({params: {connection}, ctx}) => {
+      ctx.router.push(`/dashboard/connections/${connection.id}/playground`)
     },
   }),
-};
+}
 
 export const connectionCommands = {
   'connection:delete': cmd.identity({
     icon: 'Trash',
     ..._connectionCommand,
-    execute: ({ ctx, params }) =>
+    execute: ({ctx, params}) =>
       ctx.setAlertDialogState({
         title: `Confirm deleting connection ${params.connection.id}`,
         destructive: true,
         description:
           'Already synchronized data will be untouched. However this will delete any incremental sync state so when a new connection is created you will have to sync from scratch.',
         onConfirm: () =>
-          ctx.trpcCtx.client.deleteConnection.mutate({ id: params.connection.id }),
+          ctx.trpcCtx.client.deleteConnection.mutate({
+            id: params.connection.id,
+          }),
       }),
   }),
   'connection:sync': cmd.identity({
     ..._connectionCommand,
     icon: 'RefreshCw',
-    execute: ({ params: { connection }, ctx }) => {
+    execute: ({params: {connection}, ctx}) => {
       void ctx.withToast(() =>
-        ctx.trpcCtx.client.syncConnection.mutate({ id: connection.id })
-      );
+        ctx.trpcCtx.client.syncConnection.mutate({id: connection.id}),
+      )
     },
   }),
   ...(__DEBUG__ && debugConnectionCommands),
-} satisfies CommandDefinitionMap<CommandContext>;
+} satisfies CommandDefinitionMap<CommandContext>
 
 /** Generic command that should apply to ANY entity... */
 export const entityCommands = {
   copy_id: cmd.identity({
     icon: 'Copy',
     params: z.object({
-      pipeline: z.object({ id: z.string() }).optional(),
-      connection: z.object({ id: z.string() }).optional(),
+      pipeline: z.object({id: z.string()}).optional(),
+      connection: z.object({id: z.string()}).optional(),
     }),
     useCommand: (initial) => ({
       subtitle: initial.connection?.id ?? initial.pipeline?.id,
-      execute: ({ params, ctx }) =>
+      execute: ({params, ctx}) =>
         ctx.withToast(
           () =>
             copyToClipboard(
-              initial.connection?.id ?? params.pipeline?.id ?? ''
+              initial.connection?.id ?? params.pipeline?.id ?? '',
             ),
           {
             title: 'Copied to clipboard',
-          }
+          },
         ),
     }),
   }),
-} satisfies CommandDefinitionMap<CommandContext>;
+} satisfies CommandDefinitionMap<CommandContext>
 
 // const _debugCommand = {
 //   group: 'debug',
@@ -144,48 +146,42 @@ export const entityCommands = {
 
 const _navCommand = {
   group: 'navigation',
-} satisfies CommandDefinitionInput<CommandContext>;
+} satisfies CommandDefinitionInput<CommandContext>
 
 // TODO: Dedupe with the links from the navigation sidebar
 export const navCommands = {
-  go_to_magic_link: {
+  go_to_connect: {
     ..._navCommand,
     icon: 'Wand',
-    title: 'Magic Link',
-    execute: ({ ctx }) => ctx.router.push('/dashboard/magic-link'),
-  },
-  go_to_connector_configs: {
-    ..._navCommand,
-    icon: 'Boxes',
-    title: 'Connector Configs',
-    execute: ({ ctx }) => ctx.router.push('/dashboard/connector-configs'),
+    title: 'Connect',
+    execute: ({ctx}) => ctx.router.push('/dashboard/connect'),
   },
   go_to_connections: {
     ..._navCommand,
     icon: 'Box',
     title: 'Connections',
-    execute: ({ ctx }) => ctx.router.push('/dashboard/connections'),
+    execute: ({ctx}) => ctx.router.push('/dashboard/connections'),
   },
-  go_to_api_key: {
+  go_to_connector_configs: {
     ..._navCommand,
-    icon: 'Key',
-    title: 'API Key',
-    execute: ({ ctx }) => ctx.router.push('/dashboard/api-access'),
+    icon: 'Boxes',
+    title: 'Connector Configs',
+    execute: ({ctx}) => ctx.router.push('/dashboard/connector-configs'),
+  },
+  go_to_settings: {
+    ..._navCommand,
+    icon: 'Settings',
+    execute: ({ctx}) => ctx.router.push('/dashboard/settings'),
   },
   go_to_api_docs: {
     ..._navCommand,
     icon: 'FileText',
     title: 'API Docs',
-    execute: ({ ctx }) => ctx.router.push('https://docs.openint.dev'),
+    execute: ({ctx}) => ctx.router.push('https://docs.openint.dev'),
   },
-  go_to_settings: {
-    ..._navCommand,
-    icon: 'Settings',
-    execute: ({ ctx }) => ctx.router.push('/dashboard/settings'),
-  },
-} satisfies CommandDefinitionMap<CommandContext>;
+} satisfies CommandDefinitionMap<CommandContext>
 
-export const miscCommands = {} satisfies CommandDefinitionMap<CommandContext>;
+export const miscCommands = {} satisfies CommandDefinitionMap<CommandContext>
 
 export const vDefinitions = {
   ...pipelineCommands,
@@ -194,4 +190,4 @@ export const vDefinitions = {
   ...navCommands,
   ...miscCommands,
   // ...debugCommands,
-} satisfies CommandDefinitionMap<CommandContext>;
+} satisfies CommandDefinitionMap<CommandContext>
