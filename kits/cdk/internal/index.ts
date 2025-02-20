74bf307a3b86135703421cd782d86bd22b22492f
Amadeo Pellicce <pellicceama@gmail.com>
2025-02-20 18:58:30 -0400

    exposing authtype

--- kits/cdk/internal/index.ts
+++ kits/cdk/internal/index.ts
@@ -8,3 +8,4 @@ export * from './remote-procedure'
 
 export {initNangoSDK} from '@opensdks/sdk-nango'
 export type {NangoSDK} from '@opensdks/sdk-nango'
+export {default as createNativeOauthConnect} from './oauthNative'
