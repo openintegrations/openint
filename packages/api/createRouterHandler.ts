6051edb5a7847e3a1aca72017b0f8270e5621087
Amadeo Pellicce <pellicceama@gmail.com>
2025-02-20 21:03:23 -0400

    fixing typo

--- packages/api/createRouterHandler.ts
+++ packages/api/createRouterHandler.ts
@@ -215,7 +215,7 @@ type SkipTrpcRoutes = {
 
 const skipTrpcRoutes: SkipTrpcRoutes = {
   GET: {
-    '/api/v0/unified/file-storage/files/{fileId}/download': async (
+    '/api/v0/unified/file-storage/file/{fileId}/download': async (
       req: Request,
       ctx: RouterContext,
     ) => {
