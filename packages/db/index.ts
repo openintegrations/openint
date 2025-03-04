67146276a09f873f8b26ad8714697586744e6f0e
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2025-03-04 18:00:18 +0100

    feat(v1): API V1 methods (#288)
    * connection params
    
    * implementing list connectin
    
    * adding logo_url
    
    * meta
    
    * adding pagination to events
    
    * adding pagination to ccfg
    
    * adding order by
    
    * type improvs
    
    * making input optional
    
    * note
    
    * Update packages-v1/api-v1/trpc/routers/connectorConfig.ts
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    
    * Update packages-v1/api-v1/trpc/routers/connection.ts
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    
    * adding connector utils
    
    * generalizing pagination processing
    
    * using and() for wheres, adding extend, include_secrets and refresh_policy params
    
    * moving utils
    
    * adding pagination
    
    ---------
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,6 +3,7 @@ import {type Database} from './db'
 import * as schema from './schema/schema'
 
 export * from 'drizzle-orm'
+export type {QueryBuilder} from 'drizzle-orm/pg-core'
 export * from './lib/stripeNullByte'
 export * from './lib/upsert'
 export {schema}
