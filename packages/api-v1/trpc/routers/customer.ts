e262807a597820a0b74d60ea087b3f7f5f2500da
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2025-03-06 21:38:01 +0100

    Sdk tests (#298)
    * Reenable deleted tests again
    
    * Remove packages-v1 and just have packages
    
    * remove extraneous files
    
    * fixing token creation
    
    * adding sdk test
    
    * fixing lints and depds
    
    * updating tests leaving failure occur
    
    * adding viewer endpoint
    
    * adding bearer auth for apiKey and completing tests
    
    * removing dead code
    
    * WIP docs
    
    * fixing test
    
    * tweaks
    
    * regenerating spec
    
    ---------
    
    Co-authored-by: OpenInt Bot <team@openint.dev>

--- packages/api-v1/trpc/routers/customer.ts
+++ packages/api-v1/trpc/routers/customer.ts
@@ -1,14 +1,45 @@
+import {TRPCError} from '@trpc/server'
 import {z} from 'zod'
-import {makeJwtClient} from '@openint/cdk'
+import {getServerUrl} from '@openint/app-config/constants'
+import {CustomerId, makeJwtClient, Viewer} from '@openint/cdk'
 import {publicProcedure, router} from '../_base'
-// @pellicceama fix me
-import {getServerUrl} from '../../../../apps/app-config/constants'
-import {asCustomer} from '../../../../packages/engine-backend/router/customerRouter'
 import {
   zConnectionId,
   zConnectorName,
   zCustomerId,
 } from '../utils/connectorUtils'
+
+function asCustomer(
+  viewer: Viewer,
+  input: {customerId?: CustomerId | null},
+): Viewer<'customer'> {
+  if (!('orgId' in viewer) || !viewer.orgId) {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+      message: 'Current viewer missing orgId to create token',
+    })
+  }
+  if (
+    viewer.role === 'customer' &&
+    input.customerId &&
+    input.customerId !== viewer.customerId
+  ) {
+    throw new TRPCError({
+      code: 'FORBIDDEN',
+      message: 'Current viewer cannot create token for other customer',
+    })
+  }
+  const customerId =
+    viewer.role === 'customer' ? viewer.customerId : input.customerId
+  if (!customerId) {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+      message: 'Either call as an customer or pass customerId explicitly',
+    })
+  }
+
+  return {role: 'customer', customerId, orgId: viewer.orgId}
+}
 
 export const customerRouter = router({
   createMagicLink: publicProcedure
@@ -70,6 +101,7 @@ export const customerRouter = router({
       const jwt = makeJwtClient({
         secretOrPublicKey: process.env['JWT_SECRET']!,
       })
+
       const token = jwt.signViewer(
         asCustomer(ctx.viewer, {customerId: input.customer_id as any}),
         {
