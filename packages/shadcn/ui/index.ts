11071a0a1b811edc2844f8805f815ae5316f7ed2
santiago <snrondina@gmail.com>
2025-03-11 14:24:43 -0400

    incremntal change for now

---
+++ packages/shadcn/ui/index.ts
@@ -0,0 +1,9 @@
+export * from './command'
+export * from './dialog'
+export * from './button'
+export * from './popover'
+export * from './input'
+export * from './label'
+export * from './select'
+export * from './textarea'
+// Export all other components
