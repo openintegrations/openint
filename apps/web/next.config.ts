aeb7b0b8ab693eaaf031b239f9b397d2dd0b6184
santiago <snrondina@gmail.com>
2025-03-11 22:41:15 -0400

    Live PR Review fixes

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -150,4 +150,38 @@ const nextConfig = {
   ],
 } satisfies NextConfig
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  // setCommits: {auto: true},
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  org: process.env['SENTRY_ORG'],
+  project: process.env['SENTRY_ORG'],
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env['CI'],
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: '/_sentry',
+  // tunnelRoute: "/monitoring",
+
+  tunnelRoute: '/_sentry',
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+})
