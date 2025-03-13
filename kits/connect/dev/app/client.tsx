0867145ecaad8ce828a9b2851d776049499bfffe
Amadeo Pellicce <pellicceama@gmail.com>
2025-03-13 13:20:14 -0400

    adding connect dev server

--- kits/connect/dev/app/client.tsx
+++ kits/connect/dev/app/client.tsx
@@ -1,24 +1,26 @@
 'use client'
 
+import {GetIFrameProps} from '../../src/common'
 import {OpenIntConnectEmbed} from '../../src/embed-react'
-
-interface OpenIntConnectClientProps {
-  token: string
-  baseUrl: string
-}
+import {OpenIntFrontend} from '../../src/popup'
 
 export function OpenIntConnectClient({
-  token,
   baseUrl,
-}: OpenIntConnectClientProps) {
+  params,
+}: {
+  baseUrl: string
+  params: GetIFrameProps['params']
+}) {
+  OpenIntFrontend.listenConnectEvents((event) => {
+    console.log('event3', event)
+  })
   return (
     <div className="overflow-hidden rounded-lg border">
       <OpenIntConnectEmbed
         baseUrl={baseUrl}
-        params={{
-          theme: 'light',
-          token,
-        }}
+        params={params}
+        width={700}
+        height={700}
       />
     </div>
   )
