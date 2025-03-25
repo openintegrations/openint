4f34dd2441943ba72759e490cc215ebc7fe4e09f
Rodrigo Arze <ral77ro@gmail.com>
2025-03-25 16:15:23 -0400

    Integrate delete connections endpoint in the UI

--- apps/web/app/(v1)/console/(authenticated)/connections/client.tsx
+++ apps/web/app/(v1)/console/(authenticated)/connections/client.tsx
@@ -1,8 +1,23 @@
 'use client'
 
-import {use} from 'react'
+import {Trash} from 'lucide-react'
+import {use, useMemo} from 'react'
 import {Core} from '@openint/api-v1/models'
+import {Button} from '@openint/shadcn/ui'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@openint/shadcn/ui/alert-dialog'
 import {DataTable, type ColumnDef} from '@openint/ui-v1/components/DataTable'
+import {useMutation, useSuspenseQuery} from '@openint/ui-v1/trpc'
+import {useTRPC} from '../client'
 
 const columns: ColumnDef<Core['connection']>[] = [
   {
@@ -49,12 +64,65 @@ export function ConnectionList(props: {
         offset: 0,
       }),
   )
+  const trpc = useTRPC()
+  const connectionData = useSuspenseQuery(
+    trpc.listConnections.queryOptions(
+      {},
+      initialData ? {initialData} : undefined,
+    ),
+  )
+
+  const deleteConn = useMutation(
+    trpc.deleteConnection.mutationOptions({
+      onSuccess: () => {
+        connectionData.refetch()
+      },
+    }),
+  )
+
+  const columnsWithActions = useMemo(
+    () => [
+      ...columns,
+      {
+        id: 'actions',
+        header: 'Actions',
+        cell: ({row}) => {
+          return (
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Trash className="size-6 text-red-500" />
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Delete Connection</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Are you sure you want to delete this connection? This action
+                    cannot be undone.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction
+                    onClick={() => deleteConn.mutate({id: row.original.id})}>
+                    Delete
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          )
+        },
+      } as ColumnDef<Core['connection']>,
+    ],
+    [columns, deleteConn],
+  )
 
   return (
     <div>
       <DataTable<Core['connection'], string | number | string[]>
-        data={initialData.items}
-        columns={columns}>
+        data={connectionData.data.items}
+        columns={columnsWithActions}>
         <DataTable.Header>
           <DataTable.SearchInput />
           <DataTable.ColumnVisibilityToggle />
--- apps/web/app/(v1)/console/(authenticated)/connections/page.tsx
+++ apps/web/app/(v1)/console/(authenticated)/connections/page.tsx
@@ -2,6 +2,7 @@ import {Suspense} from 'react'
 import type {PageProps} from '@/lib-common/next-utils'
 import {currentViewer} from '@/lib-server/auth.server'
 import {createAPICaller} from '@/lib-server/globals'
+import {ClientApp} from '../client'
 import {ConnectionList} from './client'
 
 // TODO: @rodri77 - Move to a shared component with a correct spinner.
@@ -10,14 +11,16 @@ function Fallback() {
 }
 
 export default async function Page(props: PageProps) {
-  const {viewer} = await currentViewer(props)
+  const {viewer, token = ''} = await currentViewer(props)
   const api = createAPICaller(viewer)
 
   return (
     <div>
-      <Suspense fallback={<Fallback />}>
-        <ConnectionList initialData={api.listConnections()} />
-      </Suspense>
+      <ClientApp token={token}>
+        <Suspense fallback={<Fallback />}>
+          <ConnectionList initialData={api.listConnections()} />
+        </Suspense>
+      </ClientApp>
     </div>
   )
 }
