11f4e64e96d6cd5e61941f0ce84e99226d508a1c
Amadeo Pellicce <pellicceama@gmail.com>
2025-03-27 11:10:49 -0700

    updating scopes

--- connectors/cnext/googlesheet/index.ts
+++ connectors/cnext/googlesheet/index.ts
@@ -21,8 +21,35 @@ const jsonDef = {
         prompt: 'consent',
       },
     },
-    openint_scopes: [],
-    scopes: [],
+    openint_scopes: [
+      'https://www.googleapis.com/auth/drive.file',
+    ],
+    scopes: [
+      {
+        scope: 'https://www.googleapis.com/auth/spreadsheets',
+        description: 'See, edit, create, and delete all your Google Sheets spreadsheets.',
+      },
+      {
+        scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
+        description: 'See all your Google Sheets spreadsheets.',
+      },
+      {
+        scope: 'https://www.googleapis.com/auth/drive',
+        description: 'See, edit, create, and delete all of your Google Drive files.',
+      },
+      {
+        scope: 'https://www.googleapis.com/auth/drive.file',
+        description: 'See, edit, create, and delete only the specific Google Drive files you use with this app.',
+      },
+      {
+        scope: 'https://www.googleapis.com/auth/drive.readonly',
+        description: 'See and download all your Google Drive files.',
+      },
+      {
+        scope: 'https://www.googleapis.com/auth/spreadsheets.currentonly',
+        description: 'Provides read/write access only to the currently opened Google Sheet. This is the most restrictive scope, limiting access to a single spreadsheet at a time.',
+      },
+    ],
   },
 } satisfies JsonConnectorDef
 
