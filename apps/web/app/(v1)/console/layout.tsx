ecfe150831bde7504130616c3a07c259d8df1321
Amadeo Pellicce <pellicceama@gmail.com>
2025-03-27 19:42:53 -0700

    wrapping clerk in dynamic layout

--- apps/web/app/(v1)/console/layout.tsx
+++ apps/web/app/(v1)/console/layout.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import {LoadingSpinner} from '@openint/engine-frontend'
 import {AuthProvider} from '@/lib-client/auth.client'
 
 export default function ConsoleLayout(props: {children: React.ReactNode}) {
-  return <AuthProvider>{props.children}</AuthProvider>
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <AuthProvider dynamic>{props.children}</AuthProvider>
+    </Suspense>
+  )
 }
