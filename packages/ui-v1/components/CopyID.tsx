016e77d9fbd81992f958b2e5dc90bcdee3f6a24f
santiago <snrondina@gmail.com>
2025-03-29 14:38:39 -0300

    vercel fix

--- packages/ui-v1/components/CopyID.tsx
+++ packages/ui-v1/components/CopyID.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {useState} from 'react'
 import {cn} from '@openint/shadcn/lib/utils'
 import {
--- packages/ui-v1/domain-components/ConnectionsCardView.tsx
+++ packages/ui-v1/domain-components/ConnectionsCardView.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, {useEffect, useState} from 'react'
 import {cn} from '@openint/shadcn/lib/utils'
 import {
