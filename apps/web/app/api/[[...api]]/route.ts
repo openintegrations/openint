78a82889f3eb9a03c367886f5f6766a6cf0d42b3
Amadeo Pellicce <pellicceama@gmail.com>
2025-03-30 11:06:26 -0700

    adding /api/v1 url

--- apps/web/app/api/[[...api]]/route.ts
+++ apps/web/app/api/[[...api]]/route.ts
@@ -2,8 +2,13 @@ import {createApp} from '@openint/api-v1'
 import {db} from '@/lib-server/globals'
 
 const app = createApp({db})
-
-const handler = app.handle
+const handler = (req: Request) => {
+  if (req.url.includes('/v1') && !req.url.includes('/api/v1')) {
+    const cleanUrl = req.url.replace('/v1', '/api/v1')
+    return app.handle(new Request(cleanUrl, req))
+  }
+  return app.handle(req)
+}
 
 export {
   handler as DELETE,
