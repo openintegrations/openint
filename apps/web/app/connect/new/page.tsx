fa889585b139a05d505dfa988e29af69ecbec612
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2025-03-30 06:25:51 +0200

    feat(console): Removing / dashboard and adding new rewrite rules for console  (#405)
    * consolidating rewrite logic
    
    * removing dashboard path
    
    * adjusting rewrite
    
    * setting correct vercel hosts

--- apps/web/app/connect/new/page.tsx
+++
@@ -1,6 +0,0 @@
-/** Embed this page to allow adding a connection */
-export default function NewPage() {
-  return (
-    <div>Embed this page to allow adding a connection</div>
-  )
-}
--- apps/web/app/connect/reconnect/page.tsx
+++
@@ -1,9 +0,0 @@
-/** Embed this page to allow user to reconnect a single connection that may have disconnected */
-export default function ReconnectPage() {
-  return (
-    <div>
-      Embed this page to allow user to reconnect a single connection that may have
-      disconnected
-    </div>
-  )
-}
