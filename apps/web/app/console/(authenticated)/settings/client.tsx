2e4ddd41b360801177d6ae6419be052ad184b144
Amadeo Pellicce <pellicceama@gmail.com>
2025-03-31 11:09:22 -0700

    refactoring webhook input form

--- apps/web/app/console/(authenticated)/settings/client.tsx
+++ apps/web/app/console/(authenticated)/settings/client.tsx
@@ -2,7 +2,7 @@
 
 import {toast} from '@openint/ui-v1'
 import SecureInput from '@openint/ui-v1/components/SecureInput'
-import WebhookInput from '@openint/ui-v1/components/WebhookInput'
+import UrlInputForm from '@openint/ui-v1/components/UrlInputForm'
 import {useMutation} from '@openint/ui-v1/trpc'
 import {useTRPC} from '../client'
 
@@ -36,20 +36,35 @@ export function SettingsContent({
   return (
     <div className="p-6">
       <h2 className="mb-4 text-2xl font-bold tracking-tight">Settings</h2>
-      <p className="mb-4 font-bold">
-        <SecureInput label="Organization Id" value={orgId} showValue={true} />
-      </p>
+      <SecureInput
+        label="Organization Id"
+        value={orgId}
+        showValue={true}
+        className="mt-4 flex items-center"
+      />
 
-      <div className="mt-4 flex items-center">
-        <SecureInput label="API Key" readOnly value={apiKey} />
-      </div>
-      <div className="mt-4 flex items-center">
-        <WebhookInput
-          defaultValue={webhookUrl}
-          onSave={handleSave}
-          isSaving={setWebhook.isPending}
-        />
-      </div>
+      <SecureInput
+        label="API Key"
+        readOnly
+        value={apiKey}
+        className="mt-4 flex items-center"
+      />
+      <UrlInputForm
+        inputName="Webhook URL"
+        defaultValue={webhookUrl}
+        placeholder="https://webhook.site/webhook-url"
+        onSave={handleSave}
+        isSaving={setWebhook.isPending}
+        className="mt-4 flex items-center"
+      />
+      {/* <UrlInputForm
+        inputName="Oauth Redirect URL"
+        defaultValue={webhookUrl}
+        placeholder="https://webhook.site/webhook-url"
+        onSave={handleSave}
+        isSaving={setWebhook.isPending}
+        className="mt-4 flex items-center"
+      /> */}
     </div>
   )
 }
--- packages/ui-v1/components/index.ts
+++ packages/ui-v1/components/index.ts
@@ -8,6 +8,6 @@ export * from './MultiSelectActionBar'
 export * from './schema-form/index'
 export * from './tabs/index'
 export * from './toast'
-export * from './WebhookInput'
+export * from './UrlInputForm'
 export * from './DataTileView'
 // codegen:end
