adb2abed8b6699c6ac7eaf2f0695bb4d78c101d4
Amadeo Pellicce <pellicceama@gmail.com>
2025-04-03 12:19:37 -0700

    evaluating is prod locally

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,5 +1,4 @@
 import {clerkMiddleware} from '@clerk/nextjs/server'
-import {isProduction} from '@openint/env'
 
 // Disable redirects
 export default clerkMiddleware()
@@ -8,6 +7,12 @@ export default clerkMiddleware()
 const config = {
   matcher: ['/console/:path*'],
 }
+
+// normally we would import isProduction from env, but this is not allowed in Edge Runtime where middleware runs
+// because of Dynamic Code Evaluation (e. g. 'eval', 'new Function', 'WebAssembly.compile') not allowed in Edge Runtime
+const isProduction =
+  process.env['NODE_ENV'] === 'production' ||
+  process.env['VERCEL_ENV'] === 'production'
 
 if (!isProduction) {
   // for dev purposes we want to be able to use connect as the console user
