1577d9f9bd4fdb49dcd80cf315255c5707002089
santiago <snrondina@gmail.com>
2025-04-08 09:33:30 -0300

    fixed assets radius

--- packages/ui-v1/domain-components/tables/ConnectionTableCell.tsx
+++ packages/ui-v1/domain-components/tables/ConnectionTableCell.tsx
@@ -22,7 +22,7 @@ export function ConnectionTableCell({
   const logo = (
     <div
       className={cn(
-        'bg-primary/15 relative flex h-[55px] w-[55px] items-center justify-center overflow-hidden rounded-md',
+        'bg-primary/15 relative flex h-[55px] w-[55px] items-center justify-center overflow-hidden rounded-sm',
         'h-8 w-8',
       )}>
       {useLogo && logo_url && (
