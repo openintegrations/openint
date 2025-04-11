2052d4f9da73c5d13125dddabadf16e220770e69
Rodrigo Arze Leon <ral77ro@gmail.com>
2025-04-11 19:23:06 -0400

    Update sharepoint and linkedin scopes (#467)
    * Add openid scope that is required for offline_access scope to work for sharepoint connector
    
    * Update basic linkedin scopes to match their latest v2 api scopes
    
    * Update snapshot scopes for linkedin and sharepoint
    
    * Update snap scopes order for linkedin

--- connectors/cnext/json-defs/sharepoint.ts
+++ connectors/cnext/json-defs/sharepoint.ts
@@ -21,12 +21,17 @@ export default {
       },
       token: {grant_type: 'authorization_code'},
     },
-    openint_scopes: ['offline_access'],
+    openint_scopes: ['offline_access', 'openid'],
     scopes: [
       {
         scope: 'offline_access',
         description:
           'Allows the application to request refresh tokens, which can be used to obtain new access tokens without user interaction. This enables the application to maintain access to resources even when the user is not actively using the application.',
+      },
+      {
+        scope: 'openid',
+        description:
+          'Enables user authentication and allows the application to receive a unique identifier for the user. This scope is required for OpenID Connect authentication flows and provides basic identity information about the authenticated user.',
       },
     ],
   },
