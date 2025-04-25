6a552d75ba12cf34fce75dc81a373368db5a0697
Rodrigo Arze <ral77ro@gmail.com>
2025-04-25 11:56:47 -0400

    Move form logic to ConnectorConfigForm domain component

--- apps/web/app/console/(authenticated)/connector-config/ConnectorConfigSheet.tsx
+++ apps/web/app/console/(authenticated)/connector-config/ConnectorConfigSheet.tsx
@@ -1,28 +1,16 @@
 'use client'
 
 import type {ConnectorConfig, Core} from '@openint/api-v1/models'
+import type {FormData} from '@openint/ui-v1'
 
-import {AlertCircle} from 'lucide-react'
-import {useCallback, useEffect, useRef, useState} from 'react'
-import {
-  Button,
-  toast,
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@openint/shadcn/ui'
+import {useState} from 'react'
 import {
   Sheet,
   SheetContent,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
 } from '@openint/shadcn/ui/sheet'
-import {
-  AddConnectorConfig,
-  getChangedFields,
-  JSONSchemaForm,
-} from '@openint/ui-v1'
+import {AddConnectorConfig, ConnectorConfigForm} from '@openint/ui-v1'
 import {useConfirm} from '@openint/ui-v1/components/Confirm'
 import {useMutation, useTRPC} from '@/lib-client/TRPCApp'
 
@@ -43,13 +31,6 @@ interface ConnectorConfigSheetProps {
   connectors: Array<Core['connector']>
 }
 
-interface FormData {
-  displayName: string
-  disabled: boolean
-  config?: Core['connector_config_insert']['config']
-  [key: string]: unknown
-}
-
 export function ConnectorConfigSheet({
   sheetOpen,
   setSheetOpen,
@@ -60,8 +41,6 @@ export function ConnectorConfigSheet({
   refetch,
   connectors,
 }: ConnectorConfigSheetProps) {
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
-  const [formState, setFormState] = useState<FormData | null>(null)
   const [changedFields, setChangedFields] = useState<string[]>([])
 
   const trpc = useTRPC()
@@ -71,68 +50,14 @@ export function ConnectorConfigSheet({
 
   const confirm = useConfirm()
 
-  const compareFormState = useCallback(() => {
-    if (selectedCcfg != null && formState) {
-      const {disabled, display_name, config} = selectedCcfg
-      return getChangedFields(formState, {
-        disabled,
-        displayName: display_name,
-        ...config,
-      })
-    }
-    return []
-  }, [selectedCcfg, formState])
-
-  useEffect(() => {
-    const newChangedFields = compareFormState()
-    setChangedFields(newChangedFields)
-  }, [compareFormState])
-
-  const initialValues = selectedCcfg
-    ? {
-        ...selectedCcfg.config,
-        displayName: selectedCcfg.display_name ?? '',
-        disabled: selectedCcfg.disabled ?? false,
-      }
-    : {}
-
-  const formContext = {
-    openint_scopes: selectedConnector?.openint_scopes ?? [],
-    scopes: selectedConnector?.scopes ?? [],
-    initialData: selectedCcfg,
-    connector: selectedConnector,
-  }
-
-  const formSchema = {
-    type: 'object' as const,
-    properties: {
-      disabled: {
-        type: 'boolean' as const,
-        title: 'Disabled',
-        description:
-          'When disabled it will not be used for connection portal. Essentially a reversible soft-delete',
-        'ui:field': 'DisabledField',
-      },
-      displayName: {
-        type: 'string' as const,
-        title: 'Display Name',
-        description: 'A friendly name for this connector configuration',
-      },
-      ...((
-        selectedConnector?.schemas?.connector_config as Record<string, unknown>
-      )?.['properties'] || {}),
-    },
-  }
-
   const discardChanges = () => {
     setSheetOpen(false)
     setSelectedConnector(null)
     setSelectedCcfg(null)
     setChangedFields([])
-    setFormState(initialValues)
   }
 
-  const saveData = async () => {
+  const saveData = async (formState: FormData) => {
     if (!formState || !selectedConnector) return
     const {displayName, disabled, config = {}, ...rest} = formState
 
@@ -144,97 +69,43 @@ export function ConnectorConfigSheet({
         ...rest,
       },
     }
-    try {
-      if (selectedCcfg) {
-        await updateConfig.mutateAsync({
-          ...baseData,
-          id: selectedCcfg.id,
-        })
-      } else {
-        await createConfig.mutateAsync({
-          ...baseData,
-          connector_name: selectedConnector.name,
-        })
-      }
 
-      toast.success(
-        `${selectedConnector?.name} ${selectedCcfg ? 'updated' : 'added'} successfully`,
-      )
-
-      await refetch()
-      setSheetOpen(false)
-      setSelectedConnector(null)
-      setSelectedCcfg(null)
-      setChangedFields([])
-    } catch (error) {
-      toast.error(
-        `Failed to save connector: ${error instanceof Error ? error.message : error}`,
-      )
-    }
-  }
-
-  const handleConfirmReconnect = async () => {
-    if (changedFields.length === 0 || !selectedConnector || !formState) return
-
-    await saveData()
-  }
-
-  const handleSave = async () => {
-    const changedFields = getChangedFields(formState, {
-      disabled: selectedCcfg?.disabled,
-      displayName: selectedCcfg?.display_name,
-      ...selectedCcfg?.config,
-    })
-    const hasOauthChanges = changedFields.some((field) => field === 'oauth')
-    if (selectedCcfg && hasOauthChanges) {
-      confirm({
-        title: 'OAuth Credentials Changed',
-        description:
-          'You have changed the OAuth credentials. This will require reconnecting any existing connections using these credentials. Are you sure you want to proceed?',
-        onConfirm: async () => {
-          await handleConfirmReconnect()
-        },
+    if (selectedCcfg) {
+      await updateConfig.mutateAsync({
+        ...baseData,
+        id: selectedCcfg.id,
       })
-      return
+    } else {
+      await createConfig.mutateAsync({
+        ...baseData,
+        connector_name: selectedConnector.name,
+      })
     }
-    await saveData()
+
+    await refetch()
+    setSheetOpen(false)
+    setSelectedConnector(null)
+    setSelectedCcfg(null)
+    setChangedFields([])
   }
 
   const handleDelete = async () => {
     if (!selectedCcfg) return
 
-    try {
-      await deleteConfig.mutateAsync({
-        id: selectedCcfg.id,
-      })
+    await deleteConfig.mutateAsync({
+      id: selectedCcfg.id,
+    })
 
-      setSheetOpen(false)
-      setSelectedConnector(null)
-      setSelectedCcfg(null)
-      toast.success(`${selectedCcfg?.connector_name} deleted successfully`)
-      await refetch()
-    } catch (error) {
-      toast.error(
-        `Failed to delete connector ${selectedCcfg?.connector_name}: ${error instanceof Error ? error.message : error}`,
-      )
-    }
+    setSheetOpen(false)
+    setSelectedConnector(null)
+    setSelectedCcfg(null)
+    await refetch()
   }
 
-  const handleSelectConnector = async (connector: Core['connector']) => {
+  const handleSelectConnector = (connector: Core['connector']) => {
     setSelectedConnector(connector)
     setSelectedCcfg(null)
   }
-
-  const handleFormChange = useCallback((data: {formData?: FormData}) => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current)
-    }
-    timeoutRef.current = setTimeout(() => {
-      if (data.formData) {
-        setFormState(data.formData)
-      }
-    }, 300)
-  }, [])
 
   const saveButtonLabel =
     createConfig.isPending || updateConfig.isPending
@@ -245,101 +116,70 @@ export function ConnectorConfigSheet({
         ? `Save ${selectedConnector?.display_name} Connector`
         : `Add ${selectedConnector?.display_name} Connector`
 
-  return (
-    <>
-      <Sheet
-        open={sheetOpen}
-        onOpenChange={(open: boolean) => {
-          if (selectedConnector && !selectedCcfg) {
-            setSelectedConnector(null)
-          } else if (selectedCcfg) {
-            if (changedFields.length > 0) {
-              confirm({
-                title: 'Discard Changes',
-                description:
-                  'You have unsaved changes. Are you sure you want to discard these changes? All information will be lost.',
-                onConfirm: discardChanges,
-              })
-              return
-            }
-            setSelectedConnector(null)
-            setSelectedCcfg(null)
-            setSheetOpen(open)
-          } else {
-            setSheetOpen(open)
-          }
-        }}>
-        <SheetContent
-          side="right"
-          className="sm:min-w-1/3 flex max-h-screen w-full flex-col overflow-hidden p-0">
-          <div className="p-4 pb-0">
-            <SheetHeader>
-              <SheetTitle className="text-lg">
-                {selectedCcfg ? 'Edit Connector' : 'Add Connector'}
-              </SheetTitle>
-            </SheetHeader>
-          </div>
+  const isSaveDisabled = createConfig.isPending || updateConfig.isPending
 
-          <div className="flex-1 overflow-y-auto">
-            {selectedConnector ? (
-              <div className="px-8">
-                <JSONSchemaForm
-                  jsonSchema={formSchema}
-                  hideSubmitButton
-                  formData={initialValues}
-                  formContext={formContext}
-                  onChange={handleFormChange}
-                />
-              </div>
-            ) : (
-              <AddConnectorConfig
-                connectors={connectors}
-                onSelectConnector={handleSelectConnector}
-              />
-            )}
-          </div>
-          {selectedConnector && (
-            <SheetFooter className="mt-auto border-t p-4">
-              <div className="flex w-full flex-row justify-between">
-                <div className="flex flex-row items-center gap-2">
-                  {selectedCcfg?.connection_count ? (
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <AlertCircle className="text-destructive size-4" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        Cannot delete connector config because it has active
-                        connections, delete the connections before deleting the
-                        connector config.
-                      </TooltipContent>
-                    </Tooltip>
-                  ) : null}
-                  <Button
-                    variant="destructive"
-                    onClick={handleDelete}
-                    disabled={
-                      !selectedCcfg ||
-                      deleteConfig.isPending ||
-                      (selectedCcfg.connection_count ?? 0) > 0
-                    }>
-                    {deleteConfig.isPending ? 'Deleting...' : 'Delete'}
-                  </Button>
-                </div>
-                <Button
-                  onClick={handleSave}
-                  disabled={
-                    createConfig.isPending ||
-                    updateConfig.isPending ||
-                    !formState ||
-                    (selectedCcfg != null && changedFields.length === 0)
-                  }>
-                  {saveButtonLabel}
-                </Button>
-              </div>
-            </SheetFooter>
+  const isDeleteDisabled =
+    !selectedCcfg ||
+    deleteConfig.isPending ||
+    (selectedCcfg.connection_count ?? 0) > 0
+
+  const onOpenChange = (open: boolean) => {
+    if (selectedConnector && !selectedCcfg) {
+      setSelectedConnector(null)
+    } else if (selectedCcfg) {
+      if (changedFields.length > 0) {
+        confirm({
+          title: 'Discard Changes',
+          description:
+            'You have unsaved changes. Are you sure you want to discard these changes? All information will be lost.',
+          onConfirm: discardChanges,
+        })
+        return
+      }
+      setSelectedConnector(null)
+      setSelectedCcfg(null)
+      setSheetOpen(open)
+    } else {
+      setSheetOpen(open)
+    }
+  }
+
+  return (
+    <Sheet open={sheetOpen} onOpenChange={onOpenChange}>
+      <SheetContent
+        side="right"
+        className="sm:min-w-1/3 flex max-h-screen w-full flex-col overflow-hidden p-0">
+        <div className="p-4 pb-0">
+          <SheetHeader>
+            <SheetTitle className="text-lg">
+              {selectedCcfg ? 'Edit Connector' : 'Add Connector'}
+            </SheetTitle>
+          </SheetHeader>
+        </div>
+
+        <div className="flex-1 overflow-y-auto">
+          {selectedConnector ? (
+            <ConnectorConfigForm
+              configSchema={selectedConnector?.schemas?.connector_config}
+              connector={selectedConnector}
+              connectorConfig={selectedCcfg}
+              onSave={saveData}
+              onDelete={handleDelete}
+              saveButtonLabel={saveButtonLabel}
+              isSaveDisabled={isSaveDisabled}
+              isDeleteDisabled={isDeleteDisabled}
+              isDeletePending={deleteConfig.isPending}
+              changedFields={changedFields}
+              setChangedFields={setChangedFields}
+            />
+          ) : (
+            <AddConnectorConfig
+              connectors={connectors}
+              onSelectConnector={handleSelectConnector}
+            />
           )}
-        </SheetContent>
-      </Sheet>
-    </>
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
--- packages/ui-v1/domain-components/ConnectorConfigForm.tsx
+++ packages/ui-v1/domain-components/ConnectorConfigForm.tsx
@@ -1,77 +1,288 @@
-import type Form from '@rjsf/core'
-import type {RJSFSchema} from '@rjsf/utils'
-import type {ForwardedRef} from 'react'
-import type {defConnectors} from '@openint/all-connectors/connectors.def'
+'use client'
 
-import {JSONSchemaForm} from '../components/schema-form'
+import type {ConnectorConfig, Core} from '@openint/api-v1/models'
 
-export interface ConnectorConfigFormProps<
-  T extends keyof typeof defConnectors,
-> {
+import {AlertCircle} from 'lucide-react'
+import {useCallback, useEffect, useRef, useState} from 'react'
+import {
+  Button,
+  toast,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@openint/shadcn/ui'
+import {useConfirm} from '../components/Confirm'
+import {getChangedFields, JSONSchemaForm} from '../components/schema-form'
+
+export interface FormData {
+  displayName: string
+  disabled: boolean
+  config?: Core['connector_config_insert']['config']
+  [key: string]: unknown
+}
+
+export interface ConnectorConfigFormProps {
   /**
-   * The name of the connector to display the config form for
-   */
-  connectorName: T
-
-  /**
-   * The connector config data to display the config form for. Initial state of the form.
-   * If data is passed in, it is expected to come in the schema for the connectorConfig.config
+   * The connector config data, Initial state of the form.
    **/
-  connectorConfig: any
-
-  jsonSchema: RJSFSchema
-
-  /**
-   * Callback for form submission
-   */
-  onSubmit: (data: any) => void
+  connectorConfig?: ConnectorConfig<
+    'connector' | 'integrations' | 'connection_count'
+  > | null
 
   /**
-   * Ref for the form element to be able to submit it.
-   * Can be created with const formRef = createRef<Form>()
-   * and submitted with formRef.current.submit();
-   * onSubmit is then called with the form data
+   * The selected connector, when we only get connector it means we are creating a new connector config
    */
-  ref: ForwardedRef<Form>
+  connector: Core['connector'] | null
+
+  configSchema: Record<string, unknown> | undefined
 
   /**
-   * Optional class name for styling the form container
+   * Callback for when the delete button is clicked
    */
-  className?: string
+  onDelete?: () => void
+
   /**
-   * Flag to indicate if the form is in a loading state
+   * Callback for when the save button is clicked
    */
-  loading?: boolean
+  onSave: (data: FormData) => Promise<void>
+
+  /**
+   * Flag to indicate if the delete button should be disabled
+   */
+  isDeleteDisabled?: boolean
+
+  /**
+   * Flag to indicate if the save button should be disabled
+   */
+  isSaveDisabled?: boolean
+
+  /**
+   * Label for the save button
+   */
+  saveButtonLabel: string
+
+  /**
+   * Flag to indicate if the delete operation is pending
+   */
+  isDeletePending?: boolean
+
+  /**
+   * changed field names
+   */
+  changedFields: string[]
+
+  /**
+   * changed fields setter
+   */
+  setChangedFields: (fields: string[]) => void
 }
 
 /**
  * ConnectorConfigForm component that displays the configuration form for a specific connector
  */
-export function ConnectorConfigForm<T extends keyof typeof defConnectors>({
-  // connectorName, // TODO: Handling the loading of the right connector config
+export function ConnectorConfigForm({
+  configSchema,
+  connector,
   connectorConfig,
-  jsonSchema,
-  className,
-  loading = false,
-  onSubmit,
-  ref,
-}: ConnectorConfigFormProps<T>) {
+  onDelete,
+  onSave,
+  isDeleteDisabled = false,
+  isSaveDisabled = false,
+  saveButtonLabel,
+  isDeletePending = false,
+  changedFields,
+  setChangedFields,
+}: ConnectorConfigFormProps) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+  const [formState, setFormState] = useState<FormData | null>(null)
+  const confirm = useConfirm()
+
+  const compareFormState = useCallback(() => {
+    if (formState) {
+      const baseValues = connectorConfig
+        ? {
+            disabled: connectorConfig.disabled,
+            displayName: connectorConfig.display_name,
+            ...connectorConfig.config,
+          }
+        : null
+      return getChangedFields(formState, baseValues)
+    }
+    return []
+  }, [connectorConfig, formState])
+
+  useEffect(() => {
+    const newChangedFields = compareFormState()
+    console.log('newChangedFields', newChangedFields)
+    setChangedFields(newChangedFields)
+  }, [compareFormState])
+
+  const initialValues = (
+    connectorConfig
+      ? {
+          ...connectorConfig.config,
+          displayName: connectorConfig.display_name ?? '',
+          disabled: connectorConfig.disabled ?? false,
+        }
+      : {}
+  ) as FormData
+
+  const formSchema = {
+    type: 'object' as const,
+    properties: {
+      disabled: {
+        type: 'boolean' as const,
+        title: 'Disabled',
+        description:
+          'When disabled it will not be used for connection portal. Essentially a reversible soft-delete',
+        'ui:field': 'DisabledField',
+      },
+      displayName: {
+        type: 'string' as const,
+        title: 'Display Name',
+        description: 'A friendly name for this connector configuration',
+      },
+      ...(configSchema?.['properties'] || {}),
+    },
+  }
+
+  const formContext = {
+    openint_scopes: connector?.openint_scopes ?? [],
+    scopes: connector?.scopes ?? [],
+    initialData: connectorConfig,
+    connector,
+  }
+
+  const handleFormChange = useCallback(
+    (data: {formData?: FormData}) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+      timeoutRef.current = setTimeout(() => {
+        if (data.formData) {
+          setFormState(data.formData)
+          const newChangedFields = getChangedFields(
+            data.formData,
+            connectorConfig
+              ? {
+                  disabled: connectorConfig.disabled,
+                  displayName: connectorConfig.display_name,
+                  ...connectorConfig.config,
+                }
+              : {
+                  disabled: false,
+                  displayName: '',
+                },
+          )
+          setChangedFields(newChangedFields)
+        }
+      }, 300)
+    },
+    [connectorConfig],
+  )
+
+  const handleSave = useCallback(async () => {
+    const saveData = async () => {
+      if (formState) {
+        try {
+          await onSave(formState)
+          toast.success(
+            `${connector?.name} ${connectorConfig ? 'updated' : 'added'} successfully`,
+          )
+        } catch (error) {
+          toast.error(
+            `Failed to save connector: ${error instanceof Error ? error.message : error}`,
+          )
+        }
+      }
+    }
+    const handleConfirmReconnect = async () => {
+      if (changedFields.length === 0 || !connector || !formState) return
+
+      await saveData()
+      setChangedFields([])
+    }
+    const hasOauthChanges = changedFields.some((field) => field === 'oauth')
+    if (connectorConfig && hasOauthChanges) {
+      confirm({
+        title: 'OAuth Credentials Changed',
+        description:
+          'You have changed the OAuth credentials. This will require reconnecting any existing connections using these credentials. Are you sure you want to proceed?',
+        onConfirm: async () => {
+          await handleConfirmReconnect()
+        },
+      })
+      return
+    }
+
+    await saveData()
+    setChangedFields([])
+  }, [
+    changedFields,
+    confirm,
+    connector,
+    connectorConfig,
+    formState,
+    onSave,
+    setChangedFields,
+  ])
+
+  const handleDelete = useCallback(async () => {
+    if (!connectorConfig || !onDelete) return
+
+    try {
+      await onDelete()
+      toast.success(`${connectorConfig?.connector_name} deleted successfully`)
+    } catch (error) {
+      toast.error(
+        `Failed to delete connector ${connectorConfig?.connector_name}: ${error instanceof Error ? error.message : error}`,
+      )
+    }
+  }, [connectorConfig, onDelete])
+
   return (
-    <div className="relative">
-      {/* TODO: Add a consistent loading indicator */}
-      {loading && (
-        <div className="absolute inset-0 z-10 flex items-center justify-center rounded bg-white/50 dark:bg-black/50">
-          <div className="border-primary h-8 w-8 animate-spin rounded-full border-4 border-t-transparent"></div>
+    <div className="flex h-full flex-col">
+      <div className="flex-1 overflow-y-auto">
+        <div className="flex h-full flex-col space-y-8 px-8">
+          <JSONSchemaForm
+            jsonSchema={formSchema}
+            formData={initialValues}
+            formContext={formContext}
+            onChange={handleFormChange}
+          />
+        </div>
+      </div>
+      {connector && (
+        <div className="mt-auto border-t p-4">
+          <div className="flex w-full flex-row justify-between">
+            <div className="flex flex-row items-center gap-2">
+              {connectorConfig?.connection_count ? (
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <AlertCircle className="text-destructive size-4" />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    Cannot delete connector config because it has active
+                    connections, delete the connections before deleting the
+                    connector config.
+                  </TooltipContent>
+                </Tooltip>
+              ) : null}
+              <Button
+                variant="destructive"
+                onClick={handleDelete}
+                disabled={isDeleteDisabled}>
+                {isDeletePending ? 'Deleting...' : 'Delete'}
+              </Button>
+            </div>
+            <Button
+              onClick={handleSave}
+              disabled={isSaveDisabled || changedFields.length === 0}>
+              {saveButtonLabel}
+            </Button>
+          </div>
         </div>
       )}
-      <JSONSchemaForm
-        ref={ref}
-        jsonSchema={jsonSchema}
-        formData={connectorConfig}
-        className={className}
-        loading={loading}
-        onSubmit={onSubmit}
-      />
     </div>
   )
 }
