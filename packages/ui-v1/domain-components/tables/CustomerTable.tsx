ea0570600320db6f2b6e14444c1fa2d5f7195c1f
Rodrigo Arze <ral77ro@gmail.com>
2025-04-29 20:06:19 -0400

    Keep using full paginated data in customers table

--- packages/ui-v1/domain-components/tables/CustomerTable.tsx
+++ packages/ui-v1/domain-components/tables/CustomerTable.tsx
@@ -73,7 +73,7 @@ export function CustomersTable({
 
   return (
     <DataTable<CustomerData, string | number>
-      data={data.items}
+      data={data}
       columns={columns}
       enableSelect={false}
       onPageChange={onPageChange}
