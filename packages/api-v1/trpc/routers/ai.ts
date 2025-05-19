30a37921885d2249c28279a99255cbe136ca4d22
Amadeo Pellicce <pellicceama@gmail.com>
2025-05-19 11:54:47 -0700

    improving prmpt to use api key

--- packages/api-v1/trpc/routers/ai.ts
+++ packages/api-v1/trpc/routers/ai.ts
@@ -25,7 +25,7 @@ export const aiRouter = router({
     )
     .output(z.object({language: z.string(), template: z.string()}))
     .query(async ({ctx, input}) => {
-      const {token} = await connectRouter
+      const {api_key} = await connectRouter
         .createCaller(ctx, {onError})
         .createToken({
           customer_id: input.customerId,
@@ -36,39 +36,40 @@ export const aiRouter = router({
           customer_id: input.customerId,
           expand: ['connector'],
         })
-      const envToken = input.useEnvironmentVariables
-        ? `process.env.OPENINT_TOKEN` // TODO: switch this based on language
-        : `'${token}'`
+      const envApiKey = input.useEnvironmentVariables
+        ? `process.env.OPENINT_API_KEY` // TODO: switch this based on language
+        : `'${api_key}'`
 
       const template = `The user uses OpenInt to manage their integrations and store the credentials for the user. The user has configured the following integrations, which are stored in the OpenInt connection settings schema.
+You can access it using the following API Key: ${api_key}
 
-You can access it using the following customer Token: ${token}
-
-Integration Name |Connector Name |Settings JSON Schema
+Integration Name|Connector Name|Settings JSON Schema (will show access token or api key at the top level)
 ${connections
   .map((connection) => {
     return `${connection.display_name}  |${connection.connector?.name}|${connection.connector?.schemas?.connection_settings}`
   })
   .join('\n')}
 
-Integrations use access tokens to authenticate. It requires you to extract the access token from OpenInt Settings and pass it to the integration. The JSON Schema for the settings is available above on how to do get an access token for an integration using OpenInt.
+Integrations use access tokens or api keys to authenticate. It requires you to extract the access token or api key from OpenInt Settings and pass it to the integration. The JSON Schema for the settings is available above on how to do get an access token or api key for an integration using OpenInt.
 
-Here's how you can do it. Ensure to also add the following comment to the code to make things easier to understand in the generated code. I'm using an example for google-calendar but refer to the table above to see what integrations are available.
+Here's how you can do it in javascript. Ensure to also add the following comments to the code to make things easier to understand in the generated code. I'm using an example for google-calendar but refer to the table above to see what integrations are available.
 
+---
 import Openint from '@openint/sdk'
 
-// Note: This has a hardcoded OpenInt customer Token set up by {OrganizationName} to manage integrations
-// You can claim access to the integration by visiting this link
-// https://openint.dev/secure/${envToken}
-const openint = new OpenInt({token: '${envToken}'})
+// Note: This has a hardcoded OpenInt API Key set up to manage integrations
+// You can manage the integration settings by visiting this link
+// https://openint.dev/manage/${envApiKey}${input.useEnvironmentVariables ? '(replace this with the value of your environment variable for OPENINT_API_KEY)' : ''}
+const openint = new OpenInt({apiKey: '${envApiKey}'})
 const connections = openInt.listConnections({connector_names: ['google-calendar'], include_secrets: true})?.items ? [];
 if(connections.length === 0) {
   throw new Error('Error fetching Google Calendar credentials, please refresh your OpenInt Token')
-  console.error("Google Calendar connection requires a refresh by visiting this url https://openint.dev/secure/${envToken}"
+  console.error("Google Calendar connection requires a refresh by visiting this url https://openint.dev/secure/${envApiKey}"
 }
-const accessToken = connection[0]?.settings?.oauth?.credentials.access_token;
-
-This access token can then be passed to the integration, either using their official SDK or their API directly. Access tokens are usually passed in the authorization header as a Bearer token but feel free to refer to the official documentation.`
+const accessToken = connection[0]?.settings?.access_token; 
+---
+This access token or api key can then be passed to the integration, either using their official SDK or their API directly. Access tokens or api keys are usually passed in the authorization header as a Bearer token but feel free to refer to the official documentation.
+`
 
       return {
         language: 'javascript',
